eclipse-egit/egit

Support spellchecking in tag dialog
Add spellchecking feature in tag message to improve user experience
during creating a tag.

Change-Id: Ic4f2ca17f59f2c6ecfefa425c1fa4decd3ab5272
Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/dialogs/commit/SpellcheckableMessageAreaTest.java
@@ -12,11 +12,11 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
-import org.eclipse.egit.ui.internal.dialogs.CommitMessageArea;
-import org.eclipse.egit.ui.internal.dialogs.CommitMessageArea.WrapEdit;
+import org.eclipse.egit.ui.internal.dialogs.SpellchecableMessageArea;
+import org.eclipse.egit.ui.internal.dialogs.SpellchecableMessageArea.WrapEdit;
 import org.junit.Test;
 
-public class CommitMessageAreaTest {
+public class SpellcheckableMessageAreaTest {
 
 	@Test
 	public void dontWrapShortText() {
@@ -130,7 +130,7 @@ public class CommitMessageAreaTest {
 
 	private static String wrap(String text, String lineDelimiter) {
 		StringBuilder sb = new StringBuilder(text);
-		List<WrapEdit> wrapEdits = CommitMessageArea.calculateWrapEdits(text, 70,
+		List<WrapEdit> wrapEdits = SpellchecableMessageArea.calculateWrapEdits(text, 70,
 				lineDelimiter);
 		for (WrapEdit wrapEdit : wrapEdits) {
 			sb.replace(wrapEdit.getStart(),

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
@@ -168,7 +168,7 @@ public class CommitDialog extends Dialog {
 				IDialogConstants.CANCEL_LABEL, false);
 	}
 
-	CommitMessageArea commitText;
+	SpellchecableMessageArea commitText;
 	Text authorText;
 	Text committerText;
 	Button amendingButton;
@@ -197,7 +197,7 @@ public class CommitDialog extends Dialog {
 		label.setText(UIText.CommitDialog_CommitMessage);
 		label.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).grab(true, false).create());
 
-		commitText = new CommitMessageArea(container, commitMessage);
+		commitText = new SpellchecableMessageArea(container, commitMessage);
 		Point size = commitText.getTextWidget().getSize();
 		int minHeight = commitText.getTextWidget().getLineHeight() * 3;
 		commitText.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).grab(true, true)

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CreateTagDialog.java
@@ -105,7 +105,7 @@ public class CreateTagDialog extends TitleAreaDialog {
 
 	private Text tagNameText;
 
-	private Text tagMessageText;
+	private SpellchecableMessageArea tagMessageText;
 
 	private Button overwriteButton;
 
@@ -376,7 +376,7 @@ public class CreateTagDialog extends TitleAreaDialog {
 
 		new Label(left, SWT.WRAP).setText(UIText.CreateTagDialog_tagMessage);
 
-		tagMessageText = new Text(left, SWT.MULTI | SWT.BORDER | SWT.V_SCROLL);
+		tagMessageText = new SpellchecableMessageArea(left, tagMessage);
 		tagMessageText.setLayoutData(GridDataFactory.fillDefaults().minSize(50,
 				50).grab(true, true).create());
 
@@ -398,7 +398,7 @@ public class CreateTagDialog extends TitleAreaDialog {
 			}
 		});
 
-		tagMessageText.addModifyListener(new ModifyListener() {
+		tagMessageText.getTextWidget().addModifyListener(new ModifyListener() {
 			public void modifyText(ModifyEvent e) {
 				validateInput();
 			}

ShoOgino/egitFile

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/dialogs/commit/SpellcheckableMessageAreaTest.java
@@ -12,11 +12,11 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
-import org.eclipse.egit.ui.internal.dialogs.CommitMessageArea;
-import org.eclipse.egit.ui.internal.dialogs.CommitMessageArea.WrapEdit;
+import org.eclipse.egit.ui.internal.dialogs.SpellcheckableMessageArea;
+import org.eclipse.egit.ui.internal.dialogs.SpellcheckableMessageArea.WrapEdit;
 import org.junit.Test;
 
-public class CommitMessageAreaTest {
+public class SpellcheckableMessageAreaTest {
 
 	@Test
 	public void dontWrapShortText() {
@@ -130,7 +130,7 @@ public class CommitMessageAreaTest {
 
 	private static String wrap(String text, String lineDelimiter) {
 		StringBuilder sb = new StringBuilder(text);
-		List<WrapEdit> wrapEdits = CommitMessageArea.calculateWrapEdits(text, 70,
+		List<WrapEdit> wrapEdits = SpellcheckableMessageArea.calculateWrapEdits(text, 70,
 				lineDelimiter);
 		for (WrapEdit wrapEdit : wrapEdits) {
 			sb.replace(wrapEdit.getStart(),

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
@@ -168,7 +168,7 @@ public class CommitDialog extends Dialog {
 				IDialogConstants.CANCEL_LABEL, false);
 	}
 
-	CommitMessageArea commitText;
+	SpellcheckableMessageArea commitText;
 	Text authorText;
 	Text committerText;
 	Button amendingButton;
@@ -197,7 +197,7 @@ public class CommitDialog extends Dialog {
 		label.setText(UIText.CommitDialog_CommitMessage);
 		label.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).grab(true, false).create());
 
-		commitText = new CommitMessageArea(container, commitMessage);
+		commitText = new SpellcheckableMessageArea(container, commitMessage);
 		Point size = commitText.getTextWidget().getSize();
 		int minHeight = commitText.getTextWidget().getLineHeight() * 3;
 		commitText.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).grab(true, true)

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CreateTagDialog.java
@@ -105,7 +105,7 @@ public class CreateTagDialog extends TitleAreaDialog {
 
 	private Text tagNameText;
 
-	private Text tagMessageText;
+	private SpellcheckableMessageArea tagMessageText;
 
 	private Button overwriteButton;
 
@@ -376,7 +376,7 @@ public class CreateTagDialog extends TitleAreaDialog {
 
 		new Label(left, SWT.WRAP).setText(UIText.CreateTagDialog_tagMessage);
 
-		tagMessageText = new Text(left, SWT.MULTI | SWT.BORDER | SWT.V_SCROLL);
+		tagMessageText = new SpellcheckableMessageArea(left, tagMessage);
 		tagMessageText.setLayoutData(GridDataFactory.fillDefaults().minSize(50,
 				50).grab(true, true).create());
 
@@ -398,7 +398,7 @@ public class CreateTagDialog extends TitleAreaDialog {
 			}
 		});
 
-		tagMessageText.addModifyListener(new ModifyListener() {
+		tagMessageText.getTextWidget().addModifyListener(new ModifyListener() {
 			public void modifyText(ModifyEvent e) {
 				validateInput();
 			}