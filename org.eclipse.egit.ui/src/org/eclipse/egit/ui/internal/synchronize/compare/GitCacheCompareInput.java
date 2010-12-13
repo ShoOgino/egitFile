eclipse-egit/egit

Fix compare input for local changes nodes.
GitModelBlob class was using compare() and hashCode() methods
implementation from GitModelCommit, therefore in some cases Compare View
was showing wrong files in comparison. It also fixes wrong headers in the
compare editor.

Bug: 326674
Change-Id: If42607deca500f3439c512382af0b13b01b90b3d
Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/compare/GitCacheCompareInput.java
@@ -1,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Dariusz Luksza <dariusz@luksza.org>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.synchronize.compare;
+
+import static org.eclipse.egit.ui.internal.CompareUtils.getFileCachedRevisionTypedElement;
+import static org.eclipse.egit.ui.internal.CompareUtils.getFileRevisionTypedElement;
+
+import org.eclipse.compare.ITypedElement;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.team.ui.mapping.ISynchronizationCompareInput;
+
+/**
+ * Specific implementation of {@link ISynchronizationCompareInput} for showing
+ * cached files
+ */
+public class GitCacheCompareInput extends GitCompareInput {
+
+	/**
+	 * Creates {@link GitCacheCompareInput}
+	 *
+	 * @param repo
+	 *            repository that is connected with this object
+	 * @param ancestorDataSource
+	 *            data that should be use to obtain common ancestor object data
+	 * @param baseDataSource
+	 *            data that should be use to obtain base object data
+	 * @param remoteDataSource
+	 *            data that should be used to obtain remote object data
+	 * @param gitPath
+	 *            repository relative path of object
+	 */
+	public GitCacheCompareInput(Repository repo,
+			ComparisonDataSource ancestorDataSource,
+			ComparisonDataSource baseDataSource,
+			ComparisonDataSource remoteDataSource, String gitPath) {
+		super(repo, ancestorDataSource, baseDataSource, remoteDataSource,
+				gitPath);
+	}
+
+	public ITypedElement getLeft() {
+		return getFileCachedRevisionTypedElement(gitPath, repo);
+	}
+
+	public ITypedElement getRight() {
+		return getFileRevisionTypedElement(gitPath, remoteCommit, repo);
+	}
+
+}

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/compare/GitCompareInput.java
@@ -8,6 +8,7 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.synchronize.compare;
 
+import static org.eclipse.egit.core.internal.storage.GitFileRevision.INDEX;
 import static org.eclipse.jgit.lib.ObjectId.zeroId;
 
 import org.eclipse.compare.CompareConfiguration;
@@ -18,6 +19,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.CompareUtils;
 import org.eclipse.egit.ui.internal.FileRevisionTypedElement;
+import org.eclipse.egit.ui.internal.LocalResourceTypedElement;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -39,15 +41,25 @@ public class GitCompareInput implements ISynchronizationCompareInput {
 
 	private final ObjectId remoteId;
 
-	private final Repository repo;
-
-	private final String gitPath;
+	private final RevCommit baseCommit;
 
 	private final RevCommit ancestorCommit;
 
-	private final RevCommit remoteCommit;
+	/**
+	 * {@link RevCommit} instance of remote commit associated with this compare
+	 * input
+	 */
+	protected final RevCommit remoteCommit;
 
-	private final RevCommit baseCommit;
+	/**
+	 * {@link Repository} associated with this compare compare input
+	 */
+	protected final Repository repo;
+
+	/**
+	 * Git repository relative path of file associated with this compare input
+	 */
+	protected final String gitPath;
 
 	/**
 	 * Creates {@link GitCompareInput}
@@ -75,8 +87,8 @@ public class GitCompareInput implements ISynchronizationCompareInput {
 		this.ancestorId = ancestroDataSource.getObjectId();
 		this.remoteCommit = remoteDataSource.getRevCommit();
 		this.ancestorCommit = ancestroDataSource.getRevCommit();
-		this.name = gitPath.lastIndexOf('/') < 0 ? gitPath :
-			gitPath.substring(gitPath.lastIndexOf('/'));
+		this.name = gitPath.lastIndexOf('/') < 0 ? gitPath : gitPath
+				.substring(gitPath.lastIndexOf('/'));
 	}
 
 	public String getName() {
@@ -140,7 +152,6 @@ public class GitCompareInput implements ISynchronizationCompareInput {
 			IProgressMonitor monitor) throws CoreException {
 		configuration.setLeftLabel(getFileRevisionLabel(getLeft()));
 		configuration.setRightLabel(getFileRevisionLabel(getRight()));
-
 	}
 
 	public String getFullPath() {
@@ -155,15 +166,24 @@ public class GitCompareInput implements ISynchronizationCompareInput {
 	private String getFileRevisionLabel(ITypedElement element) {
 		if (element instanceof FileRevisionTypedElement) {
 			FileRevisionTypedElement castElement = (FileRevisionTypedElement) element;
-			return NLS.bind(
-					UIText.GitCompareFileRevisionEditorInput_RevisionLabel,
-					new Object[] {
-							element.getName(),
-							CompareUtils.truncatedRevision(castElement
-									.getContentIdentifier()),
-							castElement.getAuthor() });
+			if (INDEX.equals(castElement.getContentIdentifier()))
+				return NLS.bind(
+						UIText.GitCompareFileRevisionEditorInput_StagedVersion,
+						element.getName());
+			else
+				return NLS.bind(
+						UIText.GitCompareFileRevisionEditorInput_RevisionLabel,
+						new Object[] {
+								element.getName(),
+								CompareUtils.truncatedRevision(castElement
+										.getContentIdentifier()),
+								castElement.getAuthor() });
 
-		} else
+		} else if (element instanceof LocalResourceTypedElement)
+			return NLS.bind(
+					UIText.GitCompareFileRevisionEditorInput_LocalVersion,
+					element.getName());
+		else
 			return element.getName();
 	}
 

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/compare/GitCacheCompareInput.java
@@ -1,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Dariusz Luksza <dariusz@luksza.org>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.synchronize.compare;
+
+import static org.eclipse.egit.ui.internal.CompareUtils.getFileCachedRevisionTypedElement;
+import static org.eclipse.egit.ui.internal.CompareUtils.getFileRevisionTypedElement;
+
+import org.eclipse.compare.ITypedElement;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.team.ui.mapping.ISynchronizationCompareInput;
+
+/**
+ * Specific implementation of {@link ISynchronizationCompareInput} for showing
+ * cached files
+ */
+public class GitCacheCompareInput extends GitCompareInput {
+
+	/**
+	 * Creates {@link GitCacheCompareInput}
+	 *
+	 * @param repo
+	 *            repository that is connected with this object
+	 * @param ancestorDataSource
+	 *            data that should be use to obtain common ancestor object data
+	 * @param baseDataSource
+	 *            data that should be use to obtain base object data
+	 * @param remoteDataSource
+	 *            data that should be used to obtain remote object data
+	 * @param gitPath
+	 *            repository relative path of object
+	 */
+	public GitCacheCompareInput(Repository repo,
+			ComparisonDataSource ancestorDataSource,
+			ComparisonDataSource baseDataSource,
+			ComparisonDataSource remoteDataSource, String gitPath) {
+		super(repo, ancestorDataSource, baseDataSource, remoteDataSource,
+				gitPath);
+	}
+
+	public ITypedElement getLeft() {
+		return getFileCachedRevisionTypedElement(gitPath, repo);
+	}
+
+	public ITypedElement getRight() {
+		return getFileRevisionTypedElement(gitPath, baseCommit, repo);
+	}
+
+}

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/compare/GitCompareInput.java
@@ -8,6 +8,7 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.synchronize.compare;
 
+import static org.eclipse.egit.core.internal.storage.GitFileRevision.INDEX;
 import static org.eclipse.jgit.lib.ObjectId.zeroId;
 
 import org.eclipse.compare.CompareConfiguration;
@@ -18,6 +19,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.CompareUtils;
 import org.eclipse.egit.ui.internal.FileRevisionTypedElement;
+import org.eclipse.egit.ui.internal.LocalResourceTypedElement;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -39,15 +41,25 @@ public class GitCompareInput implements ISynchronizationCompareInput {
 
 	private final ObjectId remoteId;
 
-	private final Repository repo;
-
-	private final String gitPath;
-
 	private final RevCommit ancestorCommit;
 
 	private final RevCommit remoteCommit;
 
-	private final RevCommit baseCommit;
+	/**
+	 * {@link RevCommit} instance of base commit associated with this compare
+	 * input
+	 */
+	protected final RevCommit baseCommit;
+
+	/**
+	 * {@link Repository} associated with this compare compare input
+	 */
+	protected final Repository repo;
+
+	/**
+	 * Git repository relative path of file associated with this compare input
+	 */
+	protected final String gitPath;
 
 	/**
 	 * Creates {@link GitCompareInput}
@@ -75,8 +87,8 @@ public class GitCompareInput implements ISynchronizationCompareInput {
 		this.ancestorId = ancestroDataSource.getObjectId();
 		this.remoteCommit = remoteDataSource.getRevCommit();
 		this.ancestorCommit = ancestroDataSource.getRevCommit();
-		this.name = gitPath.lastIndexOf('/') < 0 ? gitPath :
-			gitPath.substring(gitPath.lastIndexOf('/'));
+		this.name = gitPath.lastIndexOf('/') < 0 ? gitPath : gitPath
+				.substring(gitPath.lastIndexOf('/'));
 	}
 
 	public String getName() {
@@ -140,7 +152,6 @@ public class GitCompareInput implements ISynchronizationCompareInput {
 			IProgressMonitor monitor) throws CoreException {
 		configuration.setLeftLabel(getFileRevisionLabel(getLeft()));
 		configuration.setRightLabel(getFileRevisionLabel(getRight()));
-
 	}
 
 	public String getFullPath() {
@@ -155,15 +166,24 @@ public class GitCompareInput implements ISynchronizationCompareInput {
 	private String getFileRevisionLabel(ITypedElement element) {
 		if (element instanceof FileRevisionTypedElement) {
 			FileRevisionTypedElement castElement = (FileRevisionTypedElement) element;
-			return NLS.bind(
-					UIText.GitCompareFileRevisionEditorInput_RevisionLabel,
-					new Object[] {
-							element.getName(),
-							CompareUtils.truncatedRevision(castElement
-									.getContentIdentifier()),
-							castElement.getAuthor() });
+			if (INDEX.equals(castElement.getContentIdentifier()))
+				return NLS.bind(
+						UIText.GitCompareFileRevisionEditorInput_StagedVersion,
+						element.getName());
+			else
+				return NLS.bind(
+						UIText.GitCompareFileRevisionEditorInput_RevisionLabel,
+						new Object[] {
+								element.getName(),
+								CompareUtils.truncatedRevision(castElement
+										.getContentIdentifier()),
+								castElement.getAuthor() });
 
-		} else
+		} else if (element instanceof LocalResourceTypedElement)
+			return NLS.bind(
+					UIText.GitCompareFileRevisionEditorInput_LocalVersion,
+					element.getName());
+		else
 			return element.getName();
 	}
 