eclipse-egit/egit

Use three-way mode of the compare editor
When the user compares versions which are not direct descendants of
each other it is useful to see the content of the common ancestor.
This change introduces the usage of the three-way compare editor.

Three-way compare is still not supported when the user compares folders
or projects. This would have to be implemented in the CompareTreeView.

Change-Id: Ied1fa51d3a1ec9136ec270ff8500e08a32629fce
Signed-off-by: Stefan Lay <stefan.lay@sap.com>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2166,6 +2166,9 @@ public class UIText extends NLS {
 	public static String CompareWithPreviousActionHandler_TitleRevisionNotFound;
 
 	/** */
+	public static String CompareWithWorkingTreeHandler_errorCommonAncestor;
+
+	/** */
 	public static String ConfirmationPage_cantConnectToAnyTitle;
 
 	/** */

ShoOgino/egitFile

Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2170,6 +2170,9 @@ public class UIText extends NLS {
 	public static String CompareWithPreviousActionHandler_TitleRevisionNotFound;
 
 	/** */
+	public static String CompareUtils_errorCommonAncestor;
+
+	/** */
 	public static String ConfirmationPage_cantConnectToAnyTitle;
 
 	/** */