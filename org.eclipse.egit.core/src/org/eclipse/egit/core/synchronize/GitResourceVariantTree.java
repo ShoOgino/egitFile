eclipse-egit/egit

Enable restrained synchronization
GitSynchronizeData can now filter on a set of resources instead
of filtering on a set of containers. This finer-grained filtering
allows us to constrain the scope of synchronization operations.

Change-Id: I1624d0f4b0a24bc141d409a92bc717bf8ebe887c

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitResourceVariantTree.java
@@ -21,7 +21,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
@@ -58,8 +57,8 @@ abstract class GitResourceVariantTree extends ResourceVariantTree {
 			if (gsd.getPathFilter() == null)
 				roots.addAll(gsd.getProjects());
 			else
-				for (IContainer container : gsd.getIncludedPaths())
-					roots.add(container.getProject());
+				for (IResource resource : gsd.getIncludedPaths())
+					roots.add(resource.getProject());
 
 		return roots.toArray(new IResource[roots.size()]);
 	}

ShoOgino/egitFile

Also-by: Mikaël Barbero <mikael.barbero@obeo.fr>
Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>
Signed-off-by: Robin Stocker <robin@nibor.org>

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitResourceVariantTree.java
@@ -21,7 +21,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
@@ -58,8 +57,8 @@ abstract class GitResourceVariantTree extends ResourceVariantTree {
 			if (gsd.getPathFilter() == null)
 				roots.addAll(gsd.getProjects());
 			else
-				for (IContainer container : gsd.getIncludedPaths())
-					roots.add(container.getProject());
+				for (IResource resource : gsd.getIncludedResources())
+					roots.add(resource.getProject());
 
 		return roots.toArray(new IResource[roots.size()]);
 	}