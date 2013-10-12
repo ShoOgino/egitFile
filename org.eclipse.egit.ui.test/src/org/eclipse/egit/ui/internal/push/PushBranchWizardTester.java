eclipse-egit/egit

Push branch/initial push wizard
With certain workflows (non-Gerrit), creating a branch and pushing it to
a remote (including setup of branch tracking) is a common operation.
Currently, this is pretty complicated with EGit and a source of errors,
bug reports and help requests.

This change tries to provide a solution for the following use cases:

* The user created a new repository and wants to push it for the first
  time (probably one branch and no remotes yet).

* The user created a new feature/release branch and wants to push it to
  a remote, and set up tracking in the same step (git push -u).

It adds an additional wizard for both the above cases. For the toolbar
push action, the wizard is started when no upstream branch is configured
yet.

It's also available in the Team menu and the context menu of the
repositories view (on repo and on individual branches).

Bug: 382190
Change-Id: I7a84707e0dad78789075bdec97629c76f98c0dd7
Signed-off-by: Robin Stocker <robin@nibor.org>

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/push/PushBranchWizardTester.java
@@ -1,0 +1,112 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Robin Stocker <robin@nibor.org> and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.push;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.egit.ui.test.ContextMenuHelper;
+import org.eclipse.egit.ui.test.TestUtil;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotCombo;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+
+public class PushBranchWizardTester {
+
+	private final SWTBot wizard;
+
+	public static PushBranchWizardTester startWizard(SWTBotTree projectTree,
+			String branchName) {
+		TestUtil util = new TestUtil();
+		String pushBranchMenu = util
+				.getPluginLocalizedValue("PushBranchAction.label");
+		ContextMenuHelper.clickContextMenu(projectTree, "Team", pushBranchMenu);
+		return forBranchName(branchName);
+	}
+
+	public static PushBranchWizardTester forBranchName(String branchName) {
+		SWTWorkbenchBot bot = new SWTWorkbenchBot();
+		String title = MessageFormat.format(
+				UIText.PushBranchWizard_WindowTitle, branchName);
+		SWTBot wizard = bot.shell(title).bot();
+		return new PushBranchWizardTester(wizard);
+	}
+
+	public PushBranchWizardTester(SWTBot wizard) {
+		this.wizard = wizard;
+	}
+
+	public void selectRemote(String remoteName) {
+		SWTBotCombo remoteCombo = wizard
+				.comboBoxWithLabel(UIText.PushBranchPage_RemoteLabel);
+		String[] items = remoteCombo.items();
+		for (String item : items) {
+			if (item.startsWith(remoteName + ":")) {
+				remoteCombo.setSelection(item);
+				return;
+			}
+		}
+		throw new IllegalStateException("Could not select remote '"
+				+ remoteName + "', items were: " + Arrays.toString(items));
+	}
+
+	public void selectNewRemoteOnBranchPage(String remoteName, String uri) {
+		wizard.button(UIText.PushBranchPage_NewRemoteButton).click();
+		SWTBot addRemoteWizard = wizard.shell(UIText.AddRemoteWizard_Title)
+				.bot();
+		setRemoteNameAndUri(remoteName, uri, addRemoteWizard);
+		addRemoteWizard.button(IDialogConstants.FINISH_LABEL).click();
+	}
+
+	public void enterRemoteOnInitialPage(String remoteName, String uri) {
+		setRemoteNameAndUri(remoteName, uri, wizard);
+	}
+
+	private void setRemoteNameAndUri(String remoteName, String uri,
+			SWTBot remotePage) {
+		SWTBotText remoteNameText = remotePage
+				.textWithLabel(UIText.AddRemotePage_RemoteNameLabel);
+		remoteNameText.setText(remoteName);
+		SWTBotText uriText = remotePage
+				.textWithLabel(UIText.RepositorySelectionPage_promptURI
+				+ ":");
+		uriText.setText(uri);
+	}
+
+	public void enterBranchName(String branchName) {
+		wizard.textWithLabel(UIText.PushBranchPage_BranchNameLabel).setText(
+				branchName);
+	}
+
+	public void deselectConfigureUpstream() {
+		wizard.checkBox(UIText.PushBranchPage_ConfigureUpstreamCheck)
+				.deselect();
+	}
+
+	public void selectMerge() {
+		wizard.checkBox(UIText.PushBranchPage_ConfigureUpstreamCheck).select();
+		wizard.radio(UIText.PushBranchPage_MergeRadio).click();
+	}
+
+	public void selectRebase() {
+		wizard.checkBox(UIText.PushBranchPage_ConfigureUpstreamCheck).select();
+		wizard.radio(UIText.PushBranchPage_RebaseRadio).click();
+	}
+
+	public void next() {
+		wizard.button(IDialogConstants.NEXT_LABEL).click();
+	}
+
+	public void finish() {
+		wizard.button(IDialogConstants.FINISH_LABEL).click();
+	}
+}

ShoOgino/egitFile

Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/push/PushBranchWizardTester.java
@@ -1,0 +1,114 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Robin Stocker <robin@nibor.org> and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.push;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.egit.ui.test.ContextMenuHelper;
+import org.eclipse.egit.ui.test.TestUtil;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotCombo;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+
+public class PushBranchWizardTester {
+
+	private final SWTBot wizard;
+
+	public static PushBranchWizardTester startWizard(SWTBotTree projectTree,
+			String branchName) {
+		TestUtil util = new TestUtil();
+		String pushBranchMenu = util
+				.getPluginLocalizedValue("PushBranchAction.label");
+		ContextMenuHelper.clickContextMenu(projectTree, "Team", pushBranchMenu);
+		return forBranchName(branchName);
+	}
+
+	public static PushBranchWizardTester forBranchName(String branchName) {
+		SWTWorkbenchBot bot = new SWTWorkbenchBot();
+		String title = MessageFormat.format(
+				UIText.PushBranchWizard_WindowTitle, branchName);
+		SWTBot wizard = bot.shell(title).bot();
+		return new PushBranchWizardTester(wizard);
+	}
+
+	public PushBranchWizardTester(SWTBot wizard) {
+		this.wizard = wizard;
+	}
+
+	public void selectRemote(String remoteName) {
+		SWTBotCombo remoteCombo = wizard
+				.comboBoxWithLabel(UIText.PushBranchPage_RemoteLabel);
+		String[] items = remoteCombo.items();
+		for (String item : items) {
+			if (item.startsWith(remoteName + ":")) {
+				remoteCombo.setSelection(item);
+				return;
+			}
+		}
+		throw new IllegalStateException("Could not select remote '"
+				+ remoteName + "', items were: " + Arrays.toString(items));
+	}
+
+	public void selectNewRemoteOnBranchPage(String remoteName, String uri) {
+		wizard.button(UIText.PushBranchPage_NewRemoteButton).click();
+		SWTBot addRemoteWizard = wizard.shell(UIText.AddRemoteWizard_Title)
+				.bot();
+		setRemoteNameAndUri(remoteName, uri, addRemoteWizard);
+		addRemoteWizard.button(IDialogConstants.FINISH_LABEL).click();
+	}
+
+	public void enterRemoteOnInitialPage(String remoteName, String uri) {
+		setRemoteNameAndUri(remoteName, uri, wizard);
+	}
+
+	private void setRemoteNameAndUri(String remoteName, String uri,
+			SWTBot remotePage) {
+		SWTBotText remoteNameText = remotePage
+				.textWithLabel(UIText.AddRemotePage_RemoteNameLabel);
+		remoteNameText.setText(remoteName);
+		SWTBotText uriText = remotePage
+				.textWithLabel(UIText.RepositorySelectionPage_promptURI
+				+ ":");
+		uriText.setText(uri);
+	}
+
+	public void enterBranchName(String branchName) {
+		wizard.textWithLabel(UIText.PushBranchPage_BranchNameLabel).setText(
+				branchName);
+	}
+
+	public void deselectConfigureUpstream() {
+		wizard.checkBox(UIText.UpstreamConfigComponent_ConfigureUpstreamCheck)
+				.deselect();
+	}
+
+	public void selectMerge() {
+		wizard.checkBox(UIText.UpstreamConfigComponent_ConfigureUpstreamCheck)
+				.select();
+		wizard.radio(UIText.UpstreamConfigComponent_MergeRadio).click();
+	}
+
+	public void selectRebase() {
+		wizard.checkBox(UIText.UpstreamConfigComponent_ConfigureUpstreamCheck)
+				.select();
+		wizard.radio(UIText.UpstreamConfigComponent_RebaseRadio).click();
+	}
+
+	public void next() {
+		wizard.button(IDialogConstants.NEXT_LABEL).click();
+	}
+
+	public void finish() {
+		wizard.button(IDialogConstants.FINISH_LABEL).click();
+	}
+}