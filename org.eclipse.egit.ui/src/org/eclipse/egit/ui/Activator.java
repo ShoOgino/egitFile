eclipse-egit/egit

Prevent NPEs at plugin start and stop
PlatformUI.getWorkbench() will fail if the workbench hasn't
been initialized yet, or during shutdown. This protects
calls to it within checks for isWorkbenchRunning.

Change-Id: Iddbad2f9b8f8ba81c652d366bdc7fce71f19df7e
Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/Activator.java
@@ -35,6 +35,8 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.egit.core.RepositoryCache;
 import org.eclipse.egit.core.RepositoryUtil;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.internal.ConfigurationChecker;
@@ -234,6 +236,8 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 	}
 
 	static boolean isActive() {
+		if (!PlatformUI.isWorkbenchRunning())
+			return false;
 		final AtomicBoolean ret = new AtomicBoolean();
 		final Display display = PlatformUI.getWorkbench().getDisplay();
 		if (display.isDisposed())
@@ -267,7 +271,17 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 				refreshJob.triggerRefresh();
 			}
 		};
-		PlatformUI.getWorkbench().addWindowListener(focusListener);
+		Job job = new Job(UIText.ConfigurationChecker_checkConfiguration) {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				if (PlatformUI.isWorkbenchRunning())
+					PlatformUI.getWorkbench().addWindowListener(focusListener);
+				else
+					schedule(1000L);
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
 	}
 
 	public void optionsChanged(DebugOptions options) {
@@ -438,8 +452,14 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 
 		@Override
 		public IStatus runInWorkspace(IProgressMonitor monitor) {
-			Repository[] repos = org.eclipse.egit.core.Activator.getDefault()
-					.getRepositoryCache().getAllRepositories();
+			// The core plugin might have been stopped before we could cancel
+			// this job.
+			RepositoryCache repositoryCache = org.eclipse.egit.core.Activator
+					.getDefault().getRepositoryCache();
+			if (repositoryCache == null)
+				return Status.OK_STATUS;
+
+			Repository[] repos = repositoryCache.getAllRepositories();
 			if (repos.length == 0)
 				return Status.OK_STATUS;
 
@@ -528,7 +548,8 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 		}
 
 		if (focusListener != null) {
-			PlatformUI.getWorkbench().removeWindowListener(focusListener);
+			if (PlatformUI.isWorkbenchRunning())
+				PlatformUI.getWorkbench().removeWindowListener(focusListener);
 			focusListener = null;
 		}
 

ShoOgino/egitFile

Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/Activator.java
@@ -35,6 +35,8 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.egit.core.RepositoryCache;
 import org.eclipse.egit.core.RepositoryUtil;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.internal.ConfigurationChecker;
@@ -234,6 +236,8 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 	}
 
 	static boolean isActive() {
+		if (!PlatformUI.isWorkbenchRunning())
+			return false;
 		final AtomicBoolean ret = new AtomicBoolean();
 		final Display display = PlatformUI.getWorkbench().getDisplay();
 		if (display.isDisposed())
@@ -267,7 +271,17 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 				refreshJob.triggerRefresh();
 			}
 		};
-		PlatformUI.getWorkbench().addWindowListener(focusListener);
+		Job job = new Job(UIText.Activator_setupFocusListener) {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				if (PlatformUI.isWorkbenchRunning())
+					PlatformUI.getWorkbench().addWindowListener(focusListener);
+				else
+					schedule(1000L);
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
 	}
 
 	public void optionsChanged(DebugOptions options) {
@@ -438,8 +452,14 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 
 		@Override
 		public IStatus runInWorkspace(IProgressMonitor monitor) {
-			Repository[] repos = org.eclipse.egit.core.Activator.getDefault()
-					.getRepositoryCache().getAllRepositories();
+			// The core plugin might have been stopped before we could cancel
+			// this job.
+			RepositoryCache repositoryCache = org.eclipse.egit.core.Activator
+					.getDefault().getRepositoryCache();
+			if (repositoryCache == null)
+				return Status.OK_STATUS;
+
+			Repository[] repos = repositoryCache.getAllRepositories();
 			if (repos.length == 0)
 				return Status.OK_STATUS;
 
@@ -528,7 +548,8 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 		}
 
 		if (focusListener != null) {
-			PlatformUI.getWorkbench().removeWindowListener(focusListener);
+			if (PlatformUI.isWorkbenchRunning())
+				PlatformUI.getWorkbench().removeWindowListener(focusListener);
 			focusListener = null;
 		}
 