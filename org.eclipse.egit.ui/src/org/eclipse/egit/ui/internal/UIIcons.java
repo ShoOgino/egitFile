eclipse-egit/egit

Improve icons for interactive rebase fixup and squash actions
- use better icons provided by Vadim Dmitriev
- flip direction of the icons when reversing direction of interactive
  rebase view

Bug: 443930
Change-Id: I2d1b99d64d68a1f2d20f0519a375449d83fdd24b
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>
Also-By: Vadim Dmitriev <dmgloss@mail.ru>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIIcons.java
@@ -252,11 +252,17 @@ public class UIIcons {
 	/** Compact tree presentation icon */
 	public final static ImageDescriptor COMPACT;
 
-	/** Squash icon */
-	public final static ImageDescriptor SQUASH;
+	/** Squash icon direct order */
+	public final static ImageDescriptor SQUASH_DIRECT;
 
-	/** Fixup icon */
-	public final static ImageDescriptor FIXUP;
+	/** Squash icon reversed order */
+	public final static ImageDescriptor SQUASH_REVERSED;
+
+	/** Fixup icon direct order */
+	public final static ImageDescriptor FIXUP_DIRECT;
+
+	/** Fixup icon reversed order */
+	public final static ImageDescriptor FIXUP_REVERSED;
 
 	/** Reword icon */
 	public final static ImageDescriptor REWORD;
@@ -354,8 +360,10 @@ public class UIIcons {
 		HIERARCHY = map("elcl16/hierarchicalLayout.gif"); //$NON-NLS-1$
 		FLAT = map("elcl16/flatLayout.gif"); //$NON-NLS-1$
 		COMPACT = map("elcl16/compactLayout.gif"); //$NON-NLS-1$
-		SQUASH = map("obj16/squash.gif"); //$NON-NLS-1$
-		FIXUP = map("obj16/fixup.gif"); //$NON-NLS-1$
+		SQUASH_DIRECT = map("obj16/squash-direct.png"); //$NON-NLS-1$
+		SQUASH_REVERSED = map("obj16/squash-reversed.png"); //$NON-NLS-1$
+		FIXUP_DIRECT = map("obj16/fixup-direct.png"); //$NON-NLS-1$
+		FIXUP_REVERSED = map("obj16/fixup-reversed.png"); //$NON-NLS-1$
 		REWORD = map("obj16/reword.gif"); //$NON-NLS-1$
 		DONE_STEP = map("obj16/done_step.gif"); //$NON-NLS-1$
 		CURRENT_STEP = map("obj16/current_step.gif"); //$NON-NLS-1$

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveStepActionToolBarProvider.java
@@ -122,7 +122,7 @@ public class RebaseInteractiveStepActionToolBarProvider {
 		rebaseActionItems[2] = itemEdit;
 
 		itemSquash = new ToolItem(theToolbar, SWT.RADIO);
-		itemSquash.setImage(getImage(UIIcons.SQUASH));
+		itemSquash.setImage(getImage(UIIcons.SQUASH_DIRECT));
 		itemSquash.addSelectionListener(new ActionSelectionListener(
 				RebaseInteractivePlan.ElementAction.SQUASH));
 		itemSquash
@@ -130,7 +130,7 @@ public class RebaseInteractiveStepActionToolBarProvider {
 		rebaseActionItems[3] = itemSquash;
 
 		itemFixup = new ToolItem(theToolbar, SWT.RADIO);
-		itemFixup.setImage(getImage(UIIcons.FIXUP));
+		itemFixup.setImage(getImage(UIIcons.FIXUP_DIRECT));
 		itemFixup.addSelectionListener(new ActionSelectionListener(
 				RebaseInteractivePlan.ElementAction.FIXUP));
 		itemFixup

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveView.java
@@ -588,8 +588,8 @@ public class RebaseInteractiveView extends ViewPart implements
 			}
 		});
 
-		boolean orderReversed = RebaseInteractivePreferences.isOrderReversed();
-		int direction = (orderReversed ? SWT.DOWN : SWT.UP);
+		int direction = (RebaseInteractivePreferences.isOrderReversed() ? SWT.DOWN
+				: SWT.UP);
 
 		Tree planTree = planTreeViewer.getTree();
 		planTree.setSortColumn(stepColumn.getColumn());
@@ -606,7 +606,12 @@ public class RebaseInteractiveView extends ViewPart implements
 					case EDIT:
 						return UIIcons.getImage(resources, UIIcons.EDITCONFIG);
 					case FIXUP:
-						return UIIcons.getImage(resources, UIIcons.FIXUP);
+						if (RebaseInteractivePreferences.isOrderReversed())
+							return UIIcons.getImage(resources,
+									UIIcons.FIXUP_REVERSED);
+						else
+							return UIIcons.getImage(resources,
+									UIIcons.FIXUP_DIRECT);
 					case PICK:
 						return UIIcons.getImage(resources, UIIcons.CHERRY_PICK);
 					case REWORD:
@@ -614,7 +619,12 @@ public class RebaseInteractiveView extends ViewPart implements
 					case SKIP:
 						return UIIcons.getImage(resources, UIIcons.REBASE_SKIP);
 					case SQUASH:
-						return UIIcons.getImage(resources, UIIcons.SQUASH);
+						if (RebaseInteractivePreferences.isOrderReversed())
+							return UIIcons.getImage(resources,
+									UIIcons.SQUASH_REVERSED);
+						else
+							return UIIcons.getImage(resources,
+									UIIcons.SQUASH_DIRECT);
 					default:
 						// fall through
 					}
@@ -871,11 +881,11 @@ public class RebaseInteractiveView extends ViewPart implements
 				planViewer, actionToolBarProvider));
 		contextMenuItems.add(new PlanContextMenuAction(
 				UIText.RebaseInteractiveStepActionToolBarProvider_SquashText,
-				UIIcons.SQUASH, RebaseInteractivePlan.ElementAction.SQUASH,
+				UIIcons.SQUASH_DIRECT, RebaseInteractivePlan.ElementAction.SQUASH,
 				planViewer, actionToolBarProvider));
 		contextMenuItems.add(new PlanContextMenuAction(
 				UIText.RebaseInteractiveStepActionToolBarProvider_FixupText,
-				UIIcons.FIXUP, RebaseInteractivePlan.ElementAction.FIXUP,
+				UIIcons.FIXUP_DIRECT, RebaseInteractivePlan.ElementAction.FIXUP,
 				planViewer, actionToolBarProvider));
 		contextMenuItems.add(new PlanContextMenuAction(
 				UIText.RebaseInteractiveStepActionToolBarProvider_RewordText,

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIIcons.java
@@ -252,11 +252,17 @@ public class UIIcons {
 	/** Compact tree presentation icon */
 	public final static ImageDescriptor COMPACT;
 
-	/** Squash icon */
-	public final static ImageDescriptor SQUASH;
+	/** Squash icon arrow up */
+	public final static ImageDescriptor SQUASH_UP;
 
-	/** Fixup icon */
-	public final static ImageDescriptor FIXUP;
+	/** Squash icon arrow down */
+	public final static ImageDescriptor SQUASH_DOWN;
+
+	/** Fixup icon arrow up */
+	public final static ImageDescriptor FIXUP_UP;
+
+	/** Fixup icon arrow down */
+	public final static ImageDescriptor FIXUP_DOWN;
 
 	/** Reword icon */
 	public final static ImageDescriptor REWORD;
@@ -354,8 +360,10 @@ public class UIIcons {
 		HIERARCHY = map("elcl16/hierarchicalLayout.gif"); //$NON-NLS-1$
 		FLAT = map("elcl16/flatLayout.gif"); //$NON-NLS-1$
 		COMPACT = map("elcl16/compactLayout.gif"); //$NON-NLS-1$
-		SQUASH = map("obj16/squash.gif"); //$NON-NLS-1$
-		FIXUP = map("obj16/fixup.gif"); //$NON-NLS-1$
+		SQUASH_UP = map("obj16/squash-up.png"); //$NON-NLS-1$
+		SQUASH_DOWN = map("obj16/squash-down.png"); //$NON-NLS-1$
+		FIXUP_UP = map("obj16/fixup-up.png"); //$NON-NLS-1$
+		FIXUP_DOWN = map("obj16/fixup-down.png"); //$NON-NLS-1$
 		REWORD = map("obj16/reword.gif"); //$NON-NLS-1$
 		DONE_STEP = map("obj16/done_step.gif"); //$NON-NLS-1$
 		CURRENT_STEP = map("obj16/current_step.gif"); //$NON-NLS-1$

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveStepActionToolBarProvider.java
@@ -122,7 +122,7 @@ public class RebaseInteractiveStepActionToolBarProvider {
 		rebaseActionItems[2] = itemEdit;
 
 		itemSquash = new ToolItem(theToolbar, SWT.RADIO);
-		itemSquash.setImage(getImage(UIIcons.SQUASH));
+		itemSquash.setImage(getImage(UIIcons.SQUASH_UP));
 		itemSquash.addSelectionListener(new ActionSelectionListener(
 				RebaseInteractivePlan.ElementAction.SQUASH));
 		itemSquash
@@ -130,7 +130,7 @@ public class RebaseInteractiveStepActionToolBarProvider {
 		rebaseActionItems[3] = itemSquash;
 
 		itemFixup = new ToolItem(theToolbar, SWT.RADIO);
-		itemFixup.setImage(getImage(UIIcons.FIXUP));
+		itemFixup.setImage(getImage(UIIcons.FIXUP_UP));
 		itemFixup.addSelectionListener(new ActionSelectionListener(
 				RebaseInteractivePlan.ElementAction.FIXUP));
 		itemFixup

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveView.java
@@ -588,8 +588,8 @@ public class RebaseInteractiveView extends ViewPart implements
 			}
 		});
 
-		boolean orderReversed = RebaseInteractivePreferences.isOrderReversed();
-		int direction = (orderReversed ? SWT.DOWN : SWT.UP);
+		int direction = (RebaseInteractivePreferences.isOrderReversed() ? SWT.DOWN
+				: SWT.UP);
 
 		Tree planTree = planTreeViewer.getTree();
 		planTree.setSortColumn(stepColumn.getColumn());
@@ -606,7 +606,12 @@ public class RebaseInteractiveView extends ViewPart implements
 					case EDIT:
 						return UIIcons.getImage(resources, UIIcons.EDITCONFIG);
 					case FIXUP:
-						return UIIcons.getImage(resources, UIIcons.FIXUP);
+						if (RebaseInteractivePreferences.isOrderReversed())
+							return UIIcons.getImage(resources,
+									UIIcons.FIXUP_DOWN);
+						else
+							return UIIcons.getImage(resources,
+									UIIcons.FIXUP_UP);
 					case PICK:
 						return UIIcons.getImage(resources, UIIcons.CHERRY_PICK);
 					case REWORD:
@@ -614,7 +619,12 @@ public class RebaseInteractiveView extends ViewPart implements
 					case SKIP:
 						return UIIcons.getImage(resources, UIIcons.REBASE_SKIP);
 					case SQUASH:
-						return UIIcons.getImage(resources, UIIcons.SQUASH);
+						if (RebaseInteractivePreferences.isOrderReversed())
+							return UIIcons.getImage(resources,
+									UIIcons.SQUASH_DOWN);
+						else
+							return UIIcons.getImage(resources,
+									UIIcons.SQUASH_UP);
 					default:
 						// fall through
 					}
@@ -871,11 +881,11 @@ public class RebaseInteractiveView extends ViewPart implements
 				planViewer, actionToolBarProvider));
 		contextMenuItems.add(new PlanContextMenuAction(
 				UIText.RebaseInteractiveStepActionToolBarProvider_SquashText,
-				UIIcons.SQUASH, RebaseInteractivePlan.ElementAction.SQUASH,
+				UIIcons.SQUASH_UP, RebaseInteractivePlan.ElementAction.SQUASH,
 				planViewer, actionToolBarProvider));
 		contextMenuItems.add(new PlanContextMenuAction(
 				UIText.RebaseInteractiveStepActionToolBarProvider_FixupText,
-				UIIcons.FIXUP, RebaseInteractivePlan.ElementAction.FIXUP,
+				UIIcons.FIXUP_UP, RebaseInteractivePlan.ElementAction.FIXUP,
 				planViewer, actionToolBarProvider));
 		contextMenuItems.add(new PlanContextMenuAction(
 				UIText.RebaseInteractiveStepActionToolBarProvider_RewordText,