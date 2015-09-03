eclipse-egit/egit

Make check for empty second line configurable
Bug: 475845
Change-Id: I94146efcd7e4fdfc59af91ec11659eeb17cc8211
Signed-off-by: Christian Georgi <christian.georgi@sap.com>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
@@ -5,6 +5,7 @@
  * Copyright (C) 2012, Daniel Megert <daniel_megert@ch.ibm.com>
  * Copyright (C) 2013, Robin Stocker <robin@nibor.org>
  * Copyright (C) 2015, Jan-Ove Weichel <ovi.weichel@gmail.com>
+ * Copyright (C) 2015, SAP SE (Christian Georgi <christian.georgi@sap.com>)
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -75,6 +76,7 @@ public class PluginPreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(UIPreferences.FINDTOOLBAR_FIND_IN,
 				FindToolbar.PREFS_FINDIN_ALL);
 		store.setDefault(UIPreferences.COMMIT_DIALOG_HARD_WRAP_MESSAGE, true);
+		store.setDefault(UIPreferences.COMMIT_DIALOG_CHECK_SECOND_LINE, true);
 		store.setDefault(UIPreferences.COMMIT_DIALOG_SIGNED_OFF_BY, false);
 
 		store.setDefault(UIPreferences.REFESH_ON_INDEX_CHANGE, true);

org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIPreferences.java
@@ -7,6 +7,7 @@
  * Copyright (C) 2013, Tobias Pfeifer <to.pfeifer@sap.com>
  * Copyright (C) 2014, Marc Khouzam <marc.khouzam@ericsson.com>
  * Copyright (C) 2015, Jan-Ove Weichel <ovi.weichel@gmail.com>
+ * Copyright (C) 2015, SAP SE (Christian Georgi <christian.georgi@sap.com>)
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -65,6 +66,8 @@ public class UIPreferences {
 	public final static String FINDTOOLBAR_FIND_IN = "findtoolbar_find_in"; //$NON-NLS-1$
 	/** */
 	public final static String COMMIT_DIALOG_HARD_WRAP_MESSAGE = "commit_dialog_hard_wrap_message"; //$NON-NLS-1$
+	/** */
+	public final static String COMMIT_DIALOG_CHECK_SECOND_LINE = "commit_dialog_check_second_line"; //$NON-NLS-1$
 	/** */
 	public final static String COMMIT_DIALOG_SIGNED_OFF_BY = "commit_dialog_signed_off_by"; //$NON-NLS-1$
 	/** */

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -3162,6 +3162,12 @@ public class UIText extends NLS {
 	public static String CommittingPreferencePage_hardWrapMessageTooltip;
 
 	/** */
+	public static String CommittingPreferencePage_secondLineCheckMessage;
+
+	/** */
+	public static String CommittingPreferencePage_secondLineCheckTooltip;
+
+	/** */
 	public static String CommittingPreferencePage_footers;
 
 	/** */

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitMessageComponent.java
@@ -448,10 +448,13 @@ public class CommitMessageComponent {
 		// Check format of commit message. The soft-wrapped text in the SWT
 		// control must be converted to a hard-wrapped text, since this will be
 		// the resulting commit message.
-		String message = commitText.getCommitMessage();
-		String formatIssue = formatIssuesInCommitMessage(message);
-		if (formatIssue != null) {
-			return new CommitStatus(formatIssue, IMessageProvider.WARNING);
+		if (org.eclipse.egit.ui.Activator.getDefault().getPreferenceStore()
+				.getBoolean(UIPreferences.COMMIT_DIALOG_CHECK_SECOND_LINE)) {
+			String message = commitText.getCommitMessage();
+			String formatIssue = formatIssuesInCommitMessage(message);
+			if (formatIssue != null) {
+				return new CommitStatus(formatIssue, IMessageProvider.WARNING);
+			}
 		}
 
 		return CommitStatus.OK;

ShoOgino/egitFile

Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
@@ -5,6 +5,7 @@
  * Copyright (C) 2012, Daniel Megert <daniel_megert@ch.ibm.com>
  * Copyright (C) 2013, Robin Stocker <robin@nibor.org>
  * Copyright (C) 2015, Jan-Ove Weichel <ovi.weichel@gmail.com>
+ * Copyright (C) 2015, SAP SE (Christian Georgi <christian.georgi@sap.com>)
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -75,6 +76,7 @@ public class PluginPreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(UIPreferences.FINDTOOLBAR_FIND_IN,
 				FindToolbar.PREFS_FINDIN_ALL);
 		store.setDefault(UIPreferences.COMMIT_DIALOG_HARD_WRAP_MESSAGE, true);
+		store.setDefault(UIPreferences.COMMIT_DIALOG_WARN_ABOUT_MESSAGE_SECOND_LINE, true);
 		store.setDefault(UIPreferences.COMMIT_DIALOG_SIGNED_OFF_BY, false);
 
 		store.setDefault(UIPreferences.REFESH_ON_INDEX_CHANGE, true);

org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIPreferences.java
@@ -7,6 +7,7 @@
  * Copyright (C) 2013, Tobias Pfeifer <to.pfeifer@sap.com>
  * Copyright (C) 2014, Marc Khouzam <marc.khouzam@ericsson.com>
  * Copyright (C) 2015, Jan-Ove Weichel <ovi.weichel@gmail.com>
+ * Copyright (C) 2015, SAP SE (Christian Georgi <christian.georgi@sap.com>)
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -65,6 +66,8 @@ public class UIPreferences {
 	public final static String FINDTOOLBAR_FIND_IN = "findtoolbar_find_in"; //$NON-NLS-1$
 	/** */
 	public final static String COMMIT_DIALOG_HARD_WRAP_MESSAGE = "commit_dialog_hard_wrap_message"; //$NON-NLS-1$
+	/** */
+	public final static String COMMIT_DIALOG_WARN_ABOUT_MESSAGE_SECOND_LINE = "commit_dialog_warn_about_message_second_line"; //$NON-NLS-1$
 	/** */
 	public final static String COMMIT_DIALOG_SIGNED_OFF_BY = "commit_dialog_signed_off_by"; //$NON-NLS-1$
 	/** */

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -3172,6 +3172,12 @@ public class UIText extends NLS {
 	public static String CommittingPreferencePage_hardWrapMessageTooltip;
 
 	/** */
+	public static String CommittingPreferencePage_warnAboutCommitMessageSecondLine;
+
+	/** */
+	public static String CommittingPreferencePage_secondLineCheckTooltip;
+
+	/** */
 	public static String CommittingPreferencePage_footers;
 
 	/** */

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitMessageComponent.java
@@ -448,10 +448,13 @@ public class CommitMessageComponent {
 		// Check format of commit message. The soft-wrapped text in the SWT
 		// control must be converted to a hard-wrapped text, since this will be
 		// the resulting commit message.
-		String message = commitText.getCommitMessage();
-		String formatIssue = formatIssuesInCommitMessage(message);
-		if (formatIssue != null) {
-			return new CommitStatus(formatIssue, IMessageProvider.WARNING);
+		if (org.eclipse.egit.ui.Activator.getDefault().getPreferenceStore()
+				.getBoolean(UIPreferences.COMMIT_DIALOG_WARN_ABOUT_MESSAGE_SECOND_LINE)) {
+			String message = commitText.getCommitMessage();
+			String formatIssue = formatIssuesInCommitMessage(message);
+			if (formatIssue != null) {
+				return new CommitStatus(formatIssue, IMessageProvider.WARNING);
+			}
 		}
 
 		return CommitStatus.OK;