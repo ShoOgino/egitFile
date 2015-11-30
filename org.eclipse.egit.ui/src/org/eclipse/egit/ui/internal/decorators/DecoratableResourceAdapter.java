eclipse-egit/egit

Extract non-decoration things into new IResourceState
Separates UI-related things like branch names (actually labels) from
the state of the file in the repository and enables use of this state
elsewhere without incurring overhead for name calculations.

Bug: 355809
Change-Id: If283be82612b15d14c773eea628777eb79f263ae
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/DecoratableResourceAdapter.java
@@ -16,39 +16,24 @@
 
 package org.eclipse.egit.ui.internal.decorators;
 
-import static org.eclipse.jgit.lib.Repository.stripWorkDir;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.egit.core.internal.indexdiff.IndexDiffData;
-import org.eclipse.egit.core.internal.util.ResourceUtil;
 import org.eclipse.egit.core.project.RepositoryMapping;
+import org.eclipse.egit.ui.internal.resources.IResourceState;
+import org.eclipse.egit.ui.internal.resources.ResourceStateFactory;
 import org.eclipse.egit.ui.internal.trace.GitTraceLocation;
-import org.eclipse.jgit.annotations.Nullable;
+import org.eclipse.jgit.annotations.NonNull;
 import org.eclipse.jgit.lib.Repository;
 
 class DecoratableResourceAdapter extends DecoratableResource {
 
-	private final RepositoryMapping mapping;
-
-	private final Repository repository;
-
-	private final boolean trace;
-
-	private IndexDiffData indexDiffData;
-
-	public DecoratableResourceAdapter(IndexDiffData indexDiffData, IResource resourceToWrap)
+	public DecoratableResourceAdapter(@NonNull IndexDiffData indexDiffData,
+			@NonNull IResource resourceToWrap)
 			throws IOException {
 		super(resourceToWrap);
-		this.indexDiffData = indexDiffData;
-		trace = GitTraceLocation.DECORATION.isActive();
+		boolean trace = GitTraceLocation.DECORATION.isActive();
 		long start = 0;
 		if (trace) {
 			GitTraceLocation.getTrace().trace(
@@ -57,30 +42,29 @@ class DecoratableResourceAdapter extends DecoratableResource {
 			start = System.currentTimeMillis();
 		}
 		try {
-			mapping = RepositoryMapping.getMapping(resource);
+			RepositoryMapping mapping = RepositoryMapping
+					.getMapping(resourceToWrap);
 			if (mapping == null) {
-				repository = null;
 				return;
 			}
-			repository = mapping.getRepository();
-			if (repository == null)
+			Repository repository = mapping.getRepository();
+			if (repository == null) {
 				return;
-
-			switch (resource.getType()) {
-			case IResource.FILE:
-				extractResourceProperties();
-				break;
-			case IResource.PROJECT:
+			}
+			IResourceState baseState = ResourceStateFactory.getInstance()
+					.get(indexDiffData, resourceToWrap);
+			setTracked(baseState.isTracked());
+			setIgnored(baseState.isIgnored());
+			setDirty(baseState.isDirty());
+			setConflicts(baseState.hasConflicts());
+			setAssumeValid(baseState.isAssumeValid());
+			setStaged(baseState.staged());
+			if (resource.getType() == IResource.PROJECT) {
 				// We only need this very expensive info for project decoration
 				repositoryName = DecoratableResourceHelper
 						.getRepositoryName(repository);
 				branch = DecoratableResourceHelper.getShortBranch(repository);
 				branchStatus = DecoratableResourceHelper.getBranchStatus(repository);
-				tracked = true;
-				//$FALL-THROUGH$
-			case IResource.FOLDER:
-				extractContainerProperties();
-				break;
 			}
 		} finally {
 			if (trace)
@@ -94,144 +78,13 @@ class DecoratableResourceAdapter extends DecoratableResource {
 
 	@Override
 	public String toString() {
-		return "DecoratableResourceAdapter[" + getName() + (isTracked() ? ", tracked" : "") + (isIgnored() ? ", ignored" : "") + (isDirty() ? ", dirty" : "") + (hasConflicts() ? ",conflicts" : "") + ", staged=" + staged + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$//$NON-NLS-7$//$NON-NLS-8$//$NON-NLS-9$//$NON-NLS-10$//$NON-NLS-11$
-	}
-
-	private void extractResourceProperties() {
-		String repoRelativePath = makeRepoRelative(resource);
-
-		// ignored
-		Set<String> ignoredFiles = indexDiffData.getIgnoredNotInIndex();
-		ignored = ignoredFiles.contains(repoRelativePath)
-				|| containsPrefixPath(ignoredFiles, repoRelativePath);
-		Set<String> untracked = indexDiffData.getUntracked();
-		tracked = !untracked.contains(repoRelativePath) && !ignored;
-
-		Set<String> added = indexDiffData.getAdded();
-		Set<String> removed = indexDiffData.getRemoved();
-		Set<String> changed = indexDiffData.getChanged();
-		if (added.contains(repoRelativePath)) // added
-			staged = Staged.ADDED;
-		else if (removed.contains(repoRelativePath)) // removed
-			staged = Staged.REMOVED;
-		else if (changed.contains(repoRelativePath)) // changed and added into index
-			staged = Staged.MODIFIED;
-		else
-			staged = Staged.NOT_STAGED;
-
-		// conflicting
-		Set<String> conflicting = indexDiffData.getConflicting();
-		conflicts = conflicting.contains(repoRelativePath);
-
-		// locally modified
-		Set<String> modified = indexDiffData.getModified();
-		dirty = modified.contains(repoRelativePath);
-	}
-
-	private void extractContainerProperties() {
-		String repoRelative = makeRepoRelative(resource);
-		if (repoRelative == null)
-			return;
-		String repoRelativePath = repoRelative + "/"; //$NON-NLS-1$
-
-		if (ResourceUtil.isSymbolicLink(repository, repoRelativePath)) {
-			extractResourceProperties();
-			return;
-		}
-
-		Set<String> ignoredFiles = indexDiffData.getIgnoredNotInIndex();
-		Set<String> untrackedFolders = indexDiffData.getUntrackedFolders();
-		ignored = containsPrefixPath(ignoredFiles, repoRelativePath)
-				|| !hasContainerAnyFiles(resource);
-
-		if (ignored)
-			tracked = false;
-		else
-			tracked = !containsPrefixPath(untrackedFolders, repoRelativePath);
-
-		// containers are marked as staged whenever file was added, removed or
-		// changed
-		Set<String> changed = new HashSet<String>(indexDiffData.getChanged());
-		changed.addAll(indexDiffData.getAdded());
-		changed.addAll(indexDiffData.getRemoved());
-		if (containsPrefix(changed, repoRelativePath))
-			staged = Staged.MODIFIED;
-		else
-			staged = Staged.NOT_STAGED;
-
-		// conflicting
-		Set<String> conflicting = indexDiffData.getConflicting();
-		conflicts = containsPrefix(conflicting, repoRelativePath);
-
-		// locally modified / untracked
-		Set<String> modified = indexDiffData.getModified();
-		Set<String> untracked = indexDiffData.getUntracked();
-		Set<String> missing = indexDiffData.getMissing();
-		dirty = containsPrefix(modified, repoRelativePath)
-				|| containsPrefix(untracked, repoRelativePath)
-				|| containsPrefix(missing, repoRelativePath);
-	}
-
-	private static boolean hasContainerAnyFiles(IResource resource) {
-		if (resource instanceof IContainer) {
-			IContainer container = (IContainer) resource;
-			try {
-				return anyFile(container.members());
-			} catch (CoreException e) {
-				// if can't get any info, treat as with file
-				return true;
-			}
-		}
-		throw new IllegalArgumentException("Expected a container resource."); //$NON-NLS-1$
-	}
-
-	private static boolean anyFile(IResource[] members) {
-		for (IResource member : members) {
-			if (member.getType() == IResource.FILE)
-				return true;
-			else if (member.getType() == IResource.FOLDER)
-				if (hasContainerAnyFiles(member))
-					return true;
-		}
-		return false;
-	}
-
-	@Nullable
-	private String makeRepoRelative(IResource res) {
-		IPath location = res.getLocation();
-		if (location == null) {
-			return null;
-		}
-		if (repository.isBare()) {
-			return null;
-		}
-		File workTree = repository.getWorkTree();
-		return stripWorkDir(workTree, location.toFile());
-	}
-
-	private boolean containsPrefix(Set<String> collection, String prefix) {
-		// when prefix is empty we are handling repository root, therefore we
-		// should return true whenever collection isn't empty
-		if (prefix.length() == 1 && !collection.isEmpty())
-			return true;
-
-		for (String path : collection)
-			if (path.startsWith(prefix))
-				return true;
-		return false;
-	}
-
-	private boolean containsPrefixPath(Set<String> collection, String path) {
-		for (String entry : collection) {
-			String entryPath;
-			if (entry.endsWith("/")) //$NON-NLS-1$
-				entryPath = entry;
-			else
-				entryPath = entry + "/"; //$NON-NLS-1$
-			if (path.startsWith(entryPath))
-				return true;
-		}
-		return false;
+		return "DecoratableResourceAdapter[" + getName() //$NON-NLS-1$
+				+ (isTracked() ? ", tracked" : "") //$NON-NLS-1$ //$NON-NLS-2$
+				+ (isIgnored() ? ", ignored" : "") //$NON-NLS-1$ //$NON-NLS-2$
+				+ (isDirty() ? ", dirty" : "") //$NON-NLS-1$//$NON-NLS-2$
+				+ (hasConflicts() ? ",conflicts" : "")//$NON-NLS-1$//$NON-NLS-2$
+				+ ", staged=" + staged() //$NON-NLS-1$
+				+ "]"; //$NON-NLS-1$
 	}
 
 }

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/resources/ResourceState.java
@@ -1,0 +1,139 @@
+/*******************************************************************************
+ * Copyright (C) 2015, Thomas Wolf <thomas.wolf@paranor.ch>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.resources;
+
+import org.eclipse.jgit.annotations.NonNull;
+
+/**
+ * Base implementation of an {@link IResourceState}.
+ */
+public class ResourceState implements IResourceState {
+
+	/**
+	 * Flag indicating whether or not the resource is tracked
+	 */
+	private boolean tracked;
+
+	/**
+	 * Flag indicating whether or not the resource is ignored
+	 */
+	private boolean ignored;
+
+	/**
+	 * Flag indicating whether or not the resource has changes that are not
+	 * staged
+	 */
+	private boolean dirty;
+
+	/**
+	 * Staged state of the resource
+	 */
+	private Staged staged = Staged.NOT_STAGED;
+
+	/**
+	 * Flag indicating whether or not the resource has merge conflicts
+	 */
+	private boolean conflicts;
+
+	/**
+	 * Flag indicating whether or not the resource is assumed valid
+	 */
+	private boolean assumeValid;
+
+	@Override
+	public boolean isTracked() {
+		return tracked;
+	}
+
+	@Override
+	public boolean isIgnored() {
+		return ignored;
+	}
+
+	@Override
+	public boolean isDirty() {
+		return dirty;
+	}
+
+	@Override
+	public Staged staged() {
+		return staged;
+	}
+
+	@Override
+	public boolean hasConflicts() {
+		return conflicts;
+	}
+
+	@Override
+	public boolean isAssumeValid() {
+		return assumeValid;
+	}
+
+	/**
+	 * Sets the staged property.
+	 *
+	 * @param staged
+	 *            value to set.
+	 */
+	protected void setStaged(@NonNull Staged staged) {
+		this.staged = staged;
+	}
+
+	/**
+	 * Sets the conflicts property.
+	 *
+	 * @param conflicts
+	 *            value to set.
+	 */
+	protected void setConflicts(boolean conflicts) {
+		this.conflicts = conflicts;
+	}
+
+	/**
+	 * Sets the tracked property.
+	 *
+	 * @param tracked
+	 *            value to set.
+	 */
+	protected void setTracked(boolean tracked) {
+		this.tracked = tracked;
+	}
+
+	/**
+	 * Sets the ignored property.
+	 *
+	 * @param ignored
+	 *            value to set.
+	 */
+	protected void setIgnored(boolean ignored) {
+		this.ignored = ignored;
+	}
+
+	/**
+	 * Sets the dirty property.
+	 *
+	 * @param dirty
+	 *            value to set.
+	 */
+	protected void setDirty(boolean dirty) {
+		this.dirty = dirty;
+	}
+
+	/**
+	 * Sets the assumeValid property.
+	 *
+	 * @param assumeValid
+	 *            value to set.
+	 */
+	protected void setAssumeValid(boolean assumeValid) {
+		this.assumeValid = assumeValid;
+	}
+
+}

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/DecoratableResourceAdapter.java
@@ -7,6 +7,7 @@
  * Copyright (C) 2008, Tor Arne Vestbø <torarnv@gmail.com>
  * Copyright (C) 2011, Dariusz Luksza <dariusz@luksza.org>
  * Copyright (C) 2011, Christian Halstrick <christian.halstrick@sap.com>
+ * Copyright (C) 2015, Thomas Wolf <thomas.wolf@paranor.ch>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -16,39 +17,24 @@
 
 package org.eclipse.egit.ui.internal.decorators;
 
-import static org.eclipse.jgit.lib.Repository.stripWorkDir;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.egit.core.internal.indexdiff.IndexDiffData;
-import org.eclipse.egit.core.internal.util.ResourceUtil;
 import org.eclipse.egit.core.project.RepositoryMapping;
+import org.eclipse.egit.ui.internal.resources.IResourceState;
+import org.eclipse.egit.ui.internal.resources.ResourceStateFactory;
 import org.eclipse.egit.ui.internal.trace.GitTraceLocation;
-import org.eclipse.jgit.annotations.Nullable;
+import org.eclipse.jgit.annotations.NonNull;
 import org.eclipse.jgit.lib.Repository;
 
 class DecoratableResourceAdapter extends DecoratableResource {
 
-	private final RepositoryMapping mapping;
-
-	private final Repository repository;
-
-	private final boolean trace;
-
-	private IndexDiffData indexDiffData;
-
-	public DecoratableResourceAdapter(IndexDiffData indexDiffData, IResource resourceToWrap)
+	public DecoratableResourceAdapter(@NonNull IndexDiffData indexDiffData,
+			@NonNull IResource resourceToWrap)
 			throws IOException {
 		super(resourceToWrap);
-		this.indexDiffData = indexDiffData;
-		trace = GitTraceLocation.DECORATION.isActive();
+		boolean trace = GitTraceLocation.DECORATION.isActive();
 		long start = 0;
 		if (trace) {
 			GitTraceLocation.getTrace().trace(
@@ -57,30 +43,29 @@ class DecoratableResourceAdapter extends DecoratableResource {
 			start = System.currentTimeMillis();
 		}
 		try {
-			mapping = RepositoryMapping.getMapping(resource);
+			RepositoryMapping mapping = RepositoryMapping
+					.getMapping(resourceToWrap);
 			if (mapping == null) {
-				repository = null;
 				return;
 			}
-			repository = mapping.getRepository();
-			if (repository == null)
+			Repository repository = mapping.getRepository();
+			if (repository == null) {
 				return;
-
-			switch (resource.getType()) {
-			case IResource.FILE:
-				extractResourceProperties();
-				break;
-			case IResource.PROJECT:
+			}
+			IResourceState baseState = ResourceStateFactory.getInstance()
+					.get(indexDiffData, resourceToWrap);
+			setTracked(baseState.isTracked());
+			setIgnored(baseState.isIgnored());
+			setDirty(baseState.isDirty());
+			setConflicts(baseState.hasConflicts());
+			setAssumeValid(baseState.isAssumeValid());
+			setStagingState(baseState.getStagingState());
+			if (resource.getType() == IResource.PROJECT) {
 				// We only need this very expensive info for project decoration
 				repositoryName = DecoratableResourceHelper
 						.getRepositoryName(repository);
 				branch = DecoratableResourceHelper.getShortBranch(repository);
 				branchStatus = DecoratableResourceHelper.getBranchStatus(repository);
-				tracked = true;
-				//$FALL-THROUGH$
-			case IResource.FOLDER:
-				extractContainerProperties();
-				break;
 			}
 		} finally {
 			if (trace)
@@ -94,144 +79,13 @@ class DecoratableResourceAdapter extends DecoratableResource {
 
 	@Override
 	public String toString() {
-		return "DecoratableResourceAdapter[" + getName() + (isTracked() ? ", tracked" : "") + (isIgnored() ? ", ignored" : "") + (isDirty() ? ", dirty" : "") + (hasConflicts() ? ",conflicts" : "") + ", staged=" + staged + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$//$NON-NLS-7$//$NON-NLS-8$//$NON-NLS-9$//$NON-NLS-10$//$NON-NLS-11$
-	}
-
-	private void extractResourceProperties() {
-		String repoRelativePath = makeRepoRelative(resource);
-
-		// ignored
-		Set<String> ignoredFiles = indexDiffData.getIgnoredNotInIndex();
-		ignored = ignoredFiles.contains(repoRelativePath)
-				|| containsPrefixPath(ignoredFiles, repoRelativePath);
-		Set<String> untracked = indexDiffData.getUntracked();
-		tracked = !untracked.contains(repoRelativePath) && !ignored;
-
-		Set<String> added = indexDiffData.getAdded();
-		Set<String> removed = indexDiffData.getRemoved();
-		Set<String> changed = indexDiffData.getChanged();
-		if (added.contains(repoRelativePath)) // added
-			staged = Staged.ADDED;
-		else if (removed.contains(repoRelativePath)) // removed
-			staged = Staged.REMOVED;
-		else if (changed.contains(repoRelativePath)) // changed and added into index
-			staged = Staged.MODIFIED;
-		else
-			staged = Staged.NOT_STAGED;
-
-		// conflicting
-		Set<String> conflicting = indexDiffData.getConflicting();
-		conflicts = conflicting.contains(repoRelativePath);
-
-		// locally modified
-		Set<String> modified = indexDiffData.getModified();
-		dirty = modified.contains(repoRelativePath);
-	}
-
-	private void extractContainerProperties() {
-		String repoRelative = makeRepoRelative(resource);
-		if (repoRelative == null)
-			return;
-		String repoRelativePath = repoRelative + "/"; //$NON-NLS-1$
-
-		if (ResourceUtil.isSymbolicLink(repository, repoRelativePath)) {
-			extractResourceProperties();
-			return;
-		}
-
-		Set<String> ignoredFiles = indexDiffData.getIgnoredNotInIndex();
-		Set<String> untrackedFolders = indexDiffData.getUntrackedFolders();
-		ignored = containsPrefixPath(ignoredFiles, repoRelativePath)
-				|| !hasContainerAnyFiles(resource);
-
-		if (ignored)
-			tracked = false;
-		else
-			tracked = !containsPrefixPath(untrackedFolders, repoRelativePath);
-
-		// containers are marked as staged whenever file was added, removed or
-		// changed
-		Set<String> changed = new HashSet<String>(indexDiffData.getChanged());
-		changed.addAll(indexDiffData.getAdded());
-		changed.addAll(indexDiffData.getRemoved());
-		if (containsPrefix(changed, repoRelativePath))
-			staged = Staged.MODIFIED;
-		else
-			staged = Staged.NOT_STAGED;
-
-		// conflicting
-		Set<String> conflicting = indexDiffData.getConflicting();
-		conflicts = containsPrefix(conflicting, repoRelativePath);
-
-		// locally modified / untracked
-		Set<String> modified = indexDiffData.getModified();
-		Set<String> untracked = indexDiffData.getUntracked();
-		Set<String> missing = indexDiffData.getMissing();
-		dirty = containsPrefix(modified, repoRelativePath)
-				|| containsPrefix(untracked, repoRelativePath)
-				|| containsPrefix(missing, repoRelativePath);
-	}
-
-	private static boolean hasContainerAnyFiles(IResource resource) {
-		if (resource instanceof IContainer) {
-			IContainer container = (IContainer) resource;
-			try {
-				return anyFile(container.members());
-			} catch (CoreException e) {
-				// if can't get any info, treat as with file
-				return true;
-			}
-		}
-		throw new IllegalArgumentException("Expected a container resource."); //$NON-NLS-1$
-	}
-
-	private static boolean anyFile(IResource[] members) {
-		for (IResource member : members) {
-			if (member.getType() == IResource.FILE)
-				return true;
-			else if (member.getType() == IResource.FOLDER)
-				if (hasContainerAnyFiles(member))
-					return true;
-		}
-		return false;
-	}
-
-	@Nullable
-	private String makeRepoRelative(IResource res) {
-		IPath location = res.getLocation();
-		if (location == null) {
-			return null;
-		}
-		if (repository.isBare()) {
-			return null;
-		}
-		File workTree = repository.getWorkTree();
-		return stripWorkDir(workTree, location.toFile());
-	}
-
-	private boolean containsPrefix(Set<String> collection, String prefix) {
-		// when prefix is empty we are handling repository root, therefore we
-		// should return true whenever collection isn't empty
-		if (prefix.length() == 1 && !collection.isEmpty())
-			return true;
-
-		for (String path : collection)
-			if (path.startsWith(prefix))
-				return true;
-		return false;
-	}
-
-	private boolean containsPrefixPath(Set<String> collection, String path) {
-		for (String entry : collection) {
-			String entryPath;
-			if (entry.endsWith("/")) //$NON-NLS-1$
-				entryPath = entry;
-			else
-				entryPath = entry + "/"; //$NON-NLS-1$
-			if (path.startsWith(entryPath))
-				return true;
-		}
-		return false;
+		return "DecoratableResourceAdapter[" + getName() //$NON-NLS-1$
+				+ (isTracked() ? ", tracked" : "") //$NON-NLS-1$ //$NON-NLS-2$
+				+ (isIgnored() ? ", ignored" : "") //$NON-NLS-1$ //$NON-NLS-2$
+				+ (isDirty() ? ", dirty" : "") //$NON-NLS-1$//$NON-NLS-2$
+				+ (hasConflicts() ? ",conflicts" : "")//$NON-NLS-1$//$NON-NLS-2$
+				+ ", staged=" + getStagingState() //$NON-NLS-1$
+				+ "]"; //$NON-NLS-1$
 	}
 
 }

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/resources/ResourceState.java
@@ -1,0 +1,145 @@
+/*******************************************************************************
+ * Copyright (C) 2015, Thomas Wolf <thomas.wolf@paranor.ch>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.resources;
+
+import org.eclipse.jgit.annotations.NonNull;
+
+/**
+ * Base implementation of an {@link IResourceState}.
+ */
+public class ResourceState implements IResourceState {
+
+	/**
+	 * Flag indicating whether or not the resource is tracked
+	 */
+	private boolean tracked;
+
+	/**
+	 * Flag indicating whether or not the resource is ignored
+	 */
+	private boolean ignored;
+
+	/**
+	 * Flag indicating whether or not the resource has changes that are not
+	 * staged
+	 */
+	private boolean dirty;
+
+	/**
+	 * Staged state of the resource
+	 */
+	@NonNull
+	private StagingState staged = StagingState.NOT_STAGED;
+
+	/**
+	 * Flag indicating whether or not the resource has merge conflicts
+	 */
+	private boolean conflicts;
+
+	/**
+	 * Flag indicating whether or not the resource is assumed valid
+	 */
+	private boolean assumeValid;
+
+	@Override
+	public boolean isTracked() {
+		return tracked;
+	}
+
+	@Override
+	public boolean isIgnored() {
+		return ignored;
+	}
+
+	@Override
+	public boolean isDirty() {
+		return dirty;
+	}
+
+	@Override
+	public StagingState getStagingState() {
+		return staged;
+	}
+
+	@Override
+	public final boolean isStaged() {
+		return staged != StagingState.NOT_STAGED;
+	}
+
+	@Override
+	public boolean hasConflicts() {
+		return conflicts;
+	}
+
+	@Override
+	public boolean isAssumeValid() {
+		return assumeValid;
+	}
+
+	/**
+	 * Sets the staged property.
+	 *
+	 * @param staged
+	 *            value to set.
+	 */
+	protected void setStagingState(@NonNull StagingState staged) {
+		this.staged = staged;
+	}
+
+	/**
+	 * Sets the conflicts property.
+	 *
+	 * @param conflicts
+	 *            value to set.
+	 */
+	protected void setConflicts(boolean conflicts) {
+		this.conflicts = conflicts;
+	}
+
+	/**
+	 * Sets the tracked property.
+	 *
+	 * @param tracked
+	 *            value to set.
+	 */
+	protected void setTracked(boolean tracked) {
+		this.tracked = tracked;
+	}
+
+	/**
+	 * Sets the ignored property.
+	 *
+	 * @param ignored
+	 *            value to set.
+	 */
+	protected void setIgnored(boolean ignored) {
+		this.ignored = ignored;
+	}
+
+	/**
+	 * Sets the dirty property.
+	 *
+	 * @param dirty
+	 *            value to set.
+	 */
+	protected void setDirty(boolean dirty) {
+		this.dirty = dirty;
+	}
+
+	/**
+	 * Sets the assumeValid property.
+	 *
+	 * @param assumeValid
+	 *            value to set.
+	 */
+	protected void setAssumeValid(boolean assumeValid) {
+		this.assumeValid = assumeValid;
+	}
+
+}