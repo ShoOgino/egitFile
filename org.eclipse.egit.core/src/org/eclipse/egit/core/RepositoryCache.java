eclipse-egit/egit

Properly clean up caches when a configured repository is removed
When a repository is removed, it was never removed from the
IndexDiffCache, which in fact has no operation to remove anything. Only
the preference in ResourceUtil is updated. This of course in turn means
there's a hard reference in IndexDiffCache to the repo, and it thus also
remains in the RepositoryCache indefinitely (unless it is really
deleted, not just removed from the view).

Clean this up:

1. IndexDiffCache listens on changes to the preference storing the
   configured repositories and removes any stale cache entries upon
   changes.
2. Individual cache entries really clear all listeners, and cancel
   their jobs when disposed.
3. The RepositoryCache also listens on that preference and also removes
   stale entries proactively. Using weak references is a nice touch,
   but relying on the GC to clean up is not really necessary here.

Bug: 483664
Change-Id: I8c20d4e8821e6c5d9fa7e62191a79275bef58bce
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.egit.core/src/org/eclipse/egit/core/RepositoryCache.java
@@ -20,10 +20,14 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
@@ -34,8 +38,26 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 public class RepositoryCache {
 	private final Map<File, Reference<Repository>> repositoryCache = new HashMap<File, Reference<Repository>>();
 
+	private final IPreferenceChangeListener preferenceListener = new IPreferenceChangeListener() {
+
+		@Override
+		public void preferenceChange(PreferenceChangeEvent event) {
+			if (!RepositoryUtil.PREFS_DIRECTORIES.equals(event.getKey())) {
+				return;
+			}
+			prune(Activator.getDefault().getRepositoryUtil().getRepositories());
+		}
+
+	};
+
 	RepositoryCache() {
-		// package private constructor
+		InstanceScope.INSTANCE.getNode(Activator.getPluginId())
+				.addPreferenceChangeListener(preferenceListener);
+	}
+
+	void dispose() {
+		InstanceScope.INSTANCE.getNode(Activator.getPluginId())
+				.removePreferenceChangeListener(preferenceListener);
 	}
 
 	/**
@@ -100,8 +122,7 @@ public class RepositoryCache {
 	 * @since 3.2
 	 */
 	public Repository getRepository(final IPath location) {
-		Repository[] repositories = org.eclipse.egit.core.Activator
-				.getDefault().getRepositoryCache().getAllRepositories();
+		Repository[] repositories = getAllRepositories();
 		Repository repository = null;
 		int largestSegmentCount = 0;
 		for (Repository r : repositories) {
@@ -123,9 +144,19 @@ public class RepositoryCache {
 		for (final Iterator<Map.Entry<File, Reference<Repository>>> i = map.entrySet()
 				.iterator(); i.hasNext();) {
 			Repository repository = i.next().getValue().get();
-			if (repository == null
-					|| !repository.getDirectory().exists())
+			if (repository == null || !repository.getDirectory().exists()) {
 				i.remove();
+			}
+		}
+	}
+
+	private synchronized void prune(Set<String> configuredRepositories) {
+		Iterator<File> iterator = repositoryCache.keySet().iterator();
+		while (iterator.hasNext()) {
+			File gitDir = iterator.next();
+			if (!configuredRepositories.contains(gitDir.getAbsolutePath())) {
+				iterator.remove();
+			}
 		}
 	}
 

ShoOgino/egitFile

   stale entries proactively.

org.eclipse.egit.core/src/org/eclipse/egit/core/RepositoryCache.java
@@ -3,6 +3,7 @@
  * Copyright (C) 2008, Robin Rosenberg <robin.rosenberg@dewire.com>
  * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
  * Copyright (C) 2008, Google Inc.
+ * Copyright (C) 2016, Thomas Wolf <thomas.wolf@paranor.ch>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -20,22 +21,43 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 /**
- * Central cache for Repository instances
- *
+ * Central cache for Repository instances.
  */
 public class RepositoryCache {
 	private final Map<File, Reference<Repository>> repositoryCache = new HashMap<File, Reference<Repository>>();
 
+	private final IPreferenceChangeListener configuredRepositoriesListener = new IPreferenceChangeListener() {
+
+		@Override
+		public void preferenceChange(PreferenceChangeEvent event) {
+			if (!RepositoryUtil.PREFS_DIRECTORIES.equals(event.getKey())) {
+				return;
+			}
+			prune(Activator.getDefault().getRepositoryUtil().getRepositories());
+		}
+
+	};
+
 	RepositoryCache() {
-		// package private constructor
+		InstanceScope.INSTANCE.getNode(Activator.getPluginId())
+				.addPreferenceChangeListener(configuredRepositoriesListener);
+	}
+
+	void dispose() {
+		InstanceScope.INSTANCE.getNode(Activator.getPluginId())
+				.removePreferenceChangeListener(configuredRepositoriesListener);
 	}
 
 	/**
@@ -100,8 +122,7 @@ public class RepositoryCache {
 	 * @since 3.2
 	 */
 	public Repository getRepository(final IPath location) {
-		Repository[] repositories = org.eclipse.egit.core.Activator
-				.getDefault().getRepositoryCache().getAllRepositories();
+		Repository[] repositories = getAllRepositories();
 		Repository repository = null;
 		int largestSegmentCount = 0;
 		for (Repository r : repositories) {
@@ -123,9 +144,19 @@ public class RepositoryCache {
 		for (final Iterator<Map.Entry<File, Reference<Repository>>> i = map.entrySet()
 				.iterator(); i.hasNext();) {
 			Repository repository = i.next().getValue().get();
-			if (repository == null
-					|| !repository.getDirectory().exists())
+			if (repository == null || !repository.getDirectory().exists()) {
 				i.remove();
+			}
+		}
+	}
+
+	private synchronized void prune(Set<String> configuredRepositories) {
+		Iterator<File> iterator = repositoryCache.keySet().iterator();
+		while (iterator.hasNext()) {
+			File gitDir = iterator.next();
+			if (!configuredRepositories.contains(gitDir.getAbsolutePath())) {
+				iterator.remove();
+			}
 		}
 	}
 