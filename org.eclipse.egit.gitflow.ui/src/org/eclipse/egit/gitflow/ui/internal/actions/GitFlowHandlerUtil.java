eclipse-egit/egit

New convenience commands to compare/replace with Gitflow develop branch
When working on a feature or release branch, develop is the most common
reference point for comparison. Hence, there should be a shortcut.

Change-Id: I76e73bd6a7a9eb11a793fb2c628e7da81fa9b286
Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/actions/GitFlowHandlerUtil.java
@@ -8,12 +8,18 @@
  *******************************************************************************/
 package org.eclipse.egit.gitflow.ui.internal.actions;
 
+import java.io.IOException;
+
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.egit.gitflow.GitFlowRepository;
 import org.eclipse.egit.ui.internal.selection.SelectionUtils;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -39,4 +45,35 @@ public class GitFlowHandlerUtil {
 		return new GitFlowRepository(repository);
 	}
 
+	static IResource[] gatherResourceToOperateOn(ExecutionEvent event) {
+		return getSelectedResources(event);
+	}
+
+	static IResource[] getSelectedResources(ExecutionEvent event) {
+		IStructuredSelection selection = getSelection(event);
+		return SelectionUtils.getSelectedResources(selection);
+	}
+
+	static IStructuredSelection getSelection(ExecutionEvent event) {
+		if (event == null) {
+			throw new IllegalArgumentException("event must not be NULL"); //$NON-NLS-1$
+		}
+		Object context = event.getApplicationContext();
+		if (context instanceof IEvaluationContext) {
+			return SelectionUtils.getSelection((IEvaluationContext) context);
+		}
+		return StructuredSelection.EMPTY;
+	}
+
+	static String gatherRevision(ExecutionEvent event) throws IOException {
+		final GitFlowRepository gfRepo = GitFlowHandlerUtil
+				.getRepository(event);
+		Ref exactRef = gfRepo.getRepository()
+				.exactRef(gfRepo.getConfig().getDevelopFull());
+		if (exactRef == null) {
+			throw new IllegalStateException(
+					"Gitflow command called on non-Gitflow repository"); //$NON-NLS-1$
+		}
+		return exactRef.getName();
+	}
 }

ShoOgino/egitFile

org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/actions/GitFlowHandlerUtil.java
@@ -8,12 +8,18 @@
  *******************************************************************************/
 package org.eclipse.egit.gitflow.ui.internal.actions;
 
+import java.io.IOException;
+
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.egit.gitflow.GitFlowRepository;
 import org.eclipse.egit.ui.internal.selection.SelectionUtils;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -39,4 +45,35 @@ public class GitFlowHandlerUtil {
 		return new GitFlowRepository(repository);
 	}
 
+	static IResource[] gatherResourceToOperateOn(ExecutionEvent event) {
+		return getSelectedResources(event);
+	}
+
+	static IResource[] getSelectedResources(ExecutionEvent event) {
+		IStructuredSelection selection = getSelection(event);
+		return SelectionUtils.getSelectedResources(selection);
+	}
+
+	static IStructuredSelection getSelection(ExecutionEvent event) {
+		if (event == null) {
+			throw new IllegalArgumentException("event must not be NULL"); //$NON-NLS-1$
+		}
+		Object context = event.getApplicationContext();
+		if (context instanceof IEvaluationContext) {
+			return SelectionUtils.getSelection((IEvaluationContext) context);
+		}
+		return StructuredSelection.EMPTY;
+	}
+
+	static String gatherRevision(ExecutionEvent event) throws IOException {
+		final GitFlowRepository gfRepo = GitFlowHandlerUtil
+				.getRepository(event);
+		Ref develop = gfRepo.getRepository()
+				.exactRef(gfRepo.getConfig().getDevelopFull());
+		if (develop == null) {
+			throw new IllegalStateException(
+					"Gitflow command called on non-Gitflow repository"); //$NON-NLS-1$
+		}
+		return develop.getName();
+	}
 }