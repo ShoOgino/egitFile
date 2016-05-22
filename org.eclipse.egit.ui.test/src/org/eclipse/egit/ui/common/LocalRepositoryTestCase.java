eclipse-egit/egit

Replace uses of deprecated Repository.getRef()
Use Repository.findRef() or Repository.exactRef(), as appropriate. No
functional change.

Change-Id: Ie481c7fa5ac69d4778cf9f70ac2b019bee6e53c9
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/LocalRepositoryTestCase.java
@@ -517,7 +517,7 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 	protected static void createBranch(Repository myRepository,
 			String newRefName) throws IOException {
 		RefUpdate updateRef = myRepository.updateRef(newRefName);
-		Ref sourceBranch = myRepository.getRef("refs/heads/master");
+		Ref sourceBranch = myRepository.findRef("refs/heads/master");
 		ObjectId startAt = sourceBranch.getObjectId();
 		String startBranch = Repository.shortenRefName(sourceBranch.getName());
 		updateRef.setNewObjectId(startAt);

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/gitflow/AbstractGitflowHandlerTest.java
@@ -96,7 +96,7 @@ public abstract class AbstractGitflowHandlerTest extends LocalRepositoryTestCase
 	}
 
 	protected Ref findBranch(String branchName) throws IOException {
-		return repository.getRef(R_HEADS + branchName);
+		return repository.findRef(R_HEADS + branchName);
 	}
 
 	protected void init() throws CoreException {

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/gitflow/InitHandlerTest.java
@@ -104,7 +104,7 @@ public class InitHandlerTest extends AbstractGitflowHandlerTest {
 		assertEquals(HOTFIX_BRANCH_PREFIX, config.getHotfixPrefix());
 		assertEquals(VERSION_TAG_PREFIX, config.getVersionTagPrefix());
 
-		assertNotNull(repository.getRef(Constants.R_HEADS + DEVELOP_BRANCH));
+		assertNotNull(repository.findRef(Constants.R_HEADS + DEVELOP_BRANCH));
 	}
 
 	@Test
@@ -132,7 +132,8 @@ public class InitHandlerTest extends AbstractGitflowHandlerTest {
 		assertEquals(HOTFIX_BRANCH_PREFIX, config.getHotfixPrefix());
 		assertEquals(VERSION_TAG_PREFIX, config.getVersionTagPrefix());
 
-		assertNotNull(localRepository.getRef(Constants.R_HEADS + DEVELOP_BRANCH));
+		assertNotNull(
+				localRepository.findRef(Constants.R_HEADS + DEVELOP_BRANCH));
 	}
 
 	private void selectProject(String projectName) {

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewFetchAndPushTest.java
@@ -152,7 +152,7 @@ public class GitRepositoriesViewFetchAndPushTest extends
 		confirmed.close();
 		assertTrue("Up to date expected", uptodate);
 		// touch and run again: expect new branch
-		String objectIdBefore = repository.getRef(repository.getFullBranch())
+		String objectIdBefore = repository.findRef(repository.getFullBranch())
 				.getLeaf().getObjectId().name();
 		objectIdBefore = objectIdBefore.substring(0, 7);
 		touchAndSubmit(null);
@@ -236,7 +236,7 @@ public class GitRepositoriesViewFetchAndPushTest extends
 
 		deleteAllProjects();
 		shareProjects(clonedRepositoryFile2);
-		String objid = repository.getRef("refs/heads/master").getTarget()
+		String objid = repository.findRef("refs/heads/master").getTarget()
 				.getObjectId().name();
 		objid = objid.substring(0, 7);
 		touchAndSubmit(null);

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTestBase.java
@@ -75,7 +75,7 @@ public abstract class GitRepositoriesViewTestBase extends
 		// that we push two branches to remote
 		String newRefName = "refs/heads/stable";
 		RefUpdate updateRef = myRepository.updateRef(newRefName);
-		Ref sourceBranch = myRepository.getRef("refs/heads/master");
+		Ref sourceBranch = myRepository.findRef("refs/heads/master");
 		ObjectId startAt = sourceBranch.getObjectId();
 		String startBranch = Repository.shortenRefName(sourceBranch.getName());
 		updateRef.setNewObjectId(startAt);

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitJob.java
@@ -175,7 +175,7 @@ public class CommitJob extends Job {
 				String fullBranch = repository.getFullBranch();
 				if (fullBranch != null
 						&& fullBranch.startsWith(Constants.R_HEADS)) {
-					Ref ref = repository.getRef(fullBranch);
+					Ref ref = repository.findRef(fullBranch);
 					pushWizard = new PushBranchWizard(repository, ref);
 				} else {
 					pushWizard = new PushBranchWizard(repository,

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/CreateBranchWizard.java
@@ -60,7 +60,7 @@ public class CreateBranchWizard extends Wizard {
 				if (base.startsWith(Constants.R_HEADS)
 						|| base.startsWith(Constants.R_REMOTES)
 						|| base.startsWith(Constants.R_TAGS)) {
-					Ref currentBranch = repository.getRef(base);
+					Ref currentBranch = repository.findRef(base);
 					myPage = new CreateBranchPage(repository, currentBranch);
 				} else {
 					// the page only knows some special Refs

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesViewContentProvider.java
@@ -645,7 +645,7 @@ public class RepositoriesViewContentProvider implements ITreeContentProvider,
 	 */
 	private boolean hasStashedCommits(final Repository repository) {
 		try {
-			return repository.getRef(Constants.R_STASH) != null;
+			return repository.findRef(Constants.R_STASH) != null;
 		} catch (IOException e) {
 			return false;
 		}

ShoOgino/egitFile

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/LocalRepositoryTestCase.java
@@ -517,7 +517,7 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 	protected static void createBranch(Repository myRepository,
 			String newRefName) throws IOException {
 		RefUpdate updateRef = myRepository.updateRef(newRefName);
-		Ref sourceBranch = myRepository.getRef("refs/heads/master");
+		Ref sourceBranch = myRepository.exactRef("refs/heads/master");
 		ObjectId startAt = sourceBranch.getObjectId();
 		String startBranch = Repository.shortenRefName(sourceBranch.getName());
 		updateRef.setNewObjectId(startAt);

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/gitflow/AbstractGitflowHandlerTest.java
@@ -96,7 +96,7 @@ public abstract class AbstractGitflowHandlerTest extends LocalRepositoryTestCase
 	}
 
 	protected Ref findBranch(String branchName) throws IOException {
-		return repository.getRef(R_HEADS + branchName);
+		return repository.exactRef(R_HEADS + branchName);
 	}
 
 	protected void init() throws CoreException {

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/gitflow/InitHandlerTest.java
@@ -104,7 +104,7 @@ public class InitHandlerTest extends AbstractGitflowHandlerTest {
 		assertEquals(HOTFIX_BRANCH_PREFIX, config.getHotfixPrefix());
 		assertEquals(VERSION_TAG_PREFIX, config.getVersionTagPrefix());
 
-		assertNotNull(repository.getRef(Constants.R_HEADS + DEVELOP_BRANCH));
+		assertNotNull(repository.exactRef(Constants.R_HEADS + DEVELOP_BRANCH));
 	}
 
 	@Test
@@ -132,7 +132,8 @@ public class InitHandlerTest extends AbstractGitflowHandlerTest {
 		assertEquals(HOTFIX_BRANCH_PREFIX, config.getHotfixPrefix());
 		assertEquals(VERSION_TAG_PREFIX, config.getVersionTagPrefix());
 
-		assertNotNull(localRepository.getRef(Constants.R_HEADS + DEVELOP_BRANCH));
+		assertNotNull(
+				localRepository.exactRef(Constants.R_HEADS + DEVELOP_BRANCH));
 	}
 
 	private void selectProject(String projectName) {

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewFetchAndPushTest.java
@@ -152,7 +152,7 @@ public class GitRepositoriesViewFetchAndPushTest extends
 		confirmed.close();
 		assertTrue("Up to date expected", uptodate);
 		// touch and run again: expect new branch
-		String objectIdBefore = repository.getRef(repository.getFullBranch())
+		String objectIdBefore = repository.exactRef(repository.getFullBranch())
 				.getLeaf().getObjectId().name();
 		objectIdBefore = objectIdBefore.substring(0, 7);
 		touchAndSubmit(null);
@@ -236,7 +236,7 @@ public class GitRepositoriesViewFetchAndPushTest extends
 
 		deleteAllProjects();
 		shareProjects(clonedRepositoryFile2);
-		String objid = repository.getRef("refs/heads/master").getTarget()
+		String objid = repository.exactRef("refs/heads/master").getTarget()
 				.getObjectId().name();
 		objid = objid.substring(0, 7);
 		touchAndSubmit(null);

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTestBase.java
@@ -75,7 +75,7 @@ public abstract class GitRepositoriesViewTestBase extends
 		// that we push two branches to remote
 		String newRefName = "refs/heads/stable";
 		RefUpdate updateRef = myRepository.updateRef(newRefName);
-		Ref sourceBranch = myRepository.getRef("refs/heads/master");
+		Ref sourceBranch = myRepository.exactRef("refs/heads/master");
 		ObjectId startAt = sourceBranch.getObjectId();
 		String startBranch = Repository.shortenRefName(sourceBranch.getName());
 		updateRef.setNewObjectId(startAt);

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitJob.java
@@ -175,7 +175,7 @@ public class CommitJob extends Job {
 				String fullBranch = repository.getFullBranch();
 				if (fullBranch != null
 						&& fullBranch.startsWith(Constants.R_HEADS)) {
-					Ref ref = repository.getRef(fullBranch);
+					Ref ref = repository.exactRef(fullBranch);
 					pushWizard = new PushBranchWizard(repository, ref);
 				} else {
 					pushWizard = new PushBranchWizard(repository,

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/CreateBranchWizard.java
@@ -60,7 +60,7 @@ public class CreateBranchWizard extends Wizard {
 				if (base.startsWith(Constants.R_HEADS)
 						|| base.startsWith(Constants.R_REMOTES)
 						|| base.startsWith(Constants.R_TAGS)) {
-					Ref currentBranch = repository.getRef(base);
+					Ref currentBranch = repository.exactRef(base);
 					myPage = new CreateBranchPage(repository, currentBranch);
 				} else {
 					// the page only knows some special Refs

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesViewContentProvider.java
@@ -645,7 +645,7 @@ public class RepositoriesViewContentProvider implements ITreeContentProvider,
 	 */
 	private boolean hasStashedCommits(final Repository repository) {
 		try {
-			return repository.getRef(Constants.R_STASH) != null;
+			return repository.exactRef(Constants.R_STASH) != null;
 		} catch (IOException e) {
 			return false;
 		}