eclipse-egit/egit

Added context menu support for inner repositories.
Bug: 496356
Change-Id: If2be5ea2dd7a0216888b1bae52ed224d0640ddeb
Signed-off-by: Andre Bossert <anb0s@anbos.de>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/selection/SelectionPropertyTester.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
@@ -66,6 +67,21 @@ public class SelectionPropertyTester extends PropertyTester {
 				return false;
 
 			Object firstElement = selection.getFirstElement();
+
+			IContainer container = AdapterUtils.adapt(firstElement, IContainer.class);
+			RepositoryMapping mapping = null;
+			if (container != null) {
+				mapping = RepositoryMapping.getMapping(container);
+			}
+			if (container != null && mapping != null) {
+				if (container.equals(mapping.getContainer())) {
+					return false;
+				} else {
+					return testRepositoryProperties(mapping.getRepository(),
+							args);
+				}
+			}
+
 			IResource resource = AdapterUtils.adapt(firstElement,
 					IResource.class);
 			if ((resource != null) && (resource instanceof IFile
@@ -122,20 +138,38 @@ public class SelectionPropertyTester extends PropertyTester {
 			IProject project = AdapterUtils.adapt(element, IProject.class);
 			if (project != null) {
 				Repository r = getRepositoryOfMapping(project);
-				if (single && r != null && repo != null && r != repo)
+				if (single && r != null && repo != null && r != repo) {
 					return null;
-				else if (r != null)
+				} else if (r != null) {
 					repo = r;
+				}
 			} else {
-				IWorkingSet workingSet = AdapterUtils.adapt(element,
-						IWorkingSet.class);
-				if (workingSet != null) {
-					for (IAdaptable adaptable : workingSet.getElements()) {
-						Repository r = getRepositoryOfProject(adaptable);
-						if (single && r != null && repo != null && r != repo)
-							return null;
-						else if (r != null)
-							repo = r;
+				IContainer container = AdapterUtils.adapt(element, IContainer.class);
+				RepositoryMapping mapping = null;
+				if (container != null) {
+					mapping = RepositoryMapping.getMapping(container);
+				}
+				if (container != null && mapping != null
+						&& container.equals(mapping.getContainer())) {
+					Repository r = mapping.getRepository();
+					if (single && r != null && repo != null && r != repo) {
+						return null;
+					} else if (r != null) {
+						repo = r;
+					}
+				} else {
+					IWorkingSet workingSet = AdapterUtils.adapt(element,
+							IWorkingSet.class);
+					if (workingSet != null) {
+						for (IAdaptable adaptable : workingSet.getElements()) {
+							Repository r = getRepositoryOfProject(adaptable);
+							if (single && r != null && repo != null
+									&& r != repo) {
+								return null;
+							} else if (r != null) {
+								repo = r;
+							}
+						}
 					}
 				}
 			}

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/selection/SelectionPropertyTester.java
@@ -5,6 +5,9 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andre Bossert <anb0s@anbos.de> - Bug 496356
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.selection;
 
@@ -12,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
@@ -66,13 +70,20 @@ public class SelectionPropertyTester extends PropertyTester {
 				return false;
 
 			Object firstElement = selection.getFirstElement();
+
 			IResource resource = AdapterUtils.adapt(firstElement,
 					IResource.class);
 			if ((resource != null) && (resource instanceof IFile
 					|| resource instanceof IFolder)) {
 				RepositoryMapping m = RepositoryMapping.getMapping(resource);
 				if (m != null) {
-					return testRepositoryProperties(m.getRepository(), args);
+					if ((resource instanceof IFolder)
+							&& resource.equals(m.getContainer())) {
+						return false;
+					} else {
+						return testRepositoryProperties(m.getRepository(),
+								args);
+					}
 				}
 			}
 		} else if ("resourcesAllInRepository".equals(property)) { //$NON-NLS-1$
@@ -119,23 +130,31 @@ public class SelectionPropertyTester extends PropertyTester {
 			boolean single) {
 		Repository repo = null;
 		for (Object element : collection) {
-			IProject project = AdapterUtils.adapt(element, IProject.class);
-			if (project != null) {
-				Repository r = getRepositoryOfMapping(project);
-				if (single && r != null && repo != null && r != repo)
+			IContainer container = AdapterUtils.adapt(element,
+					IContainer.class);
+			RepositoryMapping mapping = null;
+			if (container != null) {
+				mapping = RepositoryMapping.getMapping(container);
+			}
+			if (container != null && mapping != null
+					&& container.equals(mapping.getContainer())) {
+				Repository r = mapping.getRepository();
+				if (single && r != null && repo != null && r != repo) {
 					return null;
-				else if (r != null)
+				} else if (r != null) {
 					repo = r;
+				}
 			} else {
 				IWorkingSet workingSet = AdapterUtils.adapt(element,
 						IWorkingSet.class);
 				if (workingSet != null) {
 					for (IAdaptable adaptable : workingSet.getElements()) {
 						Repository r = getRepositoryOfProject(adaptable);
-						if (single && r != null && repo != null && r != repo)
+						if (single && r != null && repo != null && r != repo) {
 							return null;
-						else if (r != null)
+						} else if (r != null) {
 							repo = r;
+						}
 					}
 				}
 			}