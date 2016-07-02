eclipse-egit/egit

Avoid opening dialogs from push/fetch background jobs
Doing so quite frequently led to UI deadlocks when modal dialogs were
active when the job's result dialog popped up. Instead use the progress
framework's mechanisms to give the user a possibility to open a job's
result dialog at his leisure. Thus there is no danger of conflicting
with any modal shells.

Push and fetch UI tests adapted to the new behavior.

Bug: 495512
Change-Id: I10f8df000fdfc4c91f3d5a9b75354caf2a37d876
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchOperationUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 SAP AG.
+ * Copyright (c) 2011, 2016 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,25 +7,27 @@
  *
  * Contributors:
  *    Mathias Kinzler (SAP AG) - initial implementation
+ *    Thomas Wolf <thomas.wolf@paranor.ch> - Bug 495512
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.fetch;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.egit.core.op.FetchOperation;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.credentials.EGitCredentialsProvider;
+import org.eclipse.egit.ui.internal.jobs.RepositoryJob;
+import org.eclipse.egit.ui.internal.jobs.RepositoryJobResultAction;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jgit.annotations.NonNull;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.FetchResult;
@@ -34,6 +36,8 @@ import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.TagOpt;
 import org.eclipse.jgit.transport.URIish;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * UI Wrapper for {@link FetchOperation}
@@ -113,14 +117,20 @@ public class FetchOperationUI {
 	 * completion
 	 */
 	public void start() {
-		Job job = new WorkspaceJob(NLS.bind(
+		final Repository repo = repository;
+		if (repo == null) {
+			return;
+		}
+		Job job = new RepositoryJob(NLS.bind(
 				UIText.FetchOperationUI_FetchJobName,
 				sourceString)) {
 
+			private FetchResult result;
+
 			@Override
-			public IStatus runInWorkspace(IProgressMonitor monitor) {
+			public IStatus performJob(IProgressMonitor monitor) {
 				try {
-					execute(monitor);
+					result = execute(monitor);
 				} catch (CoreException e) {
 					return Activator.createErrorStatus(e.getStatus()
 							.getMessage(), e);
@@ -129,25 +139,20 @@ public class FetchOperationUI {
 			}
 
 			@Override
+			protected Action getAction() {
+				return new ShowResultAction(repo, result, sourceString);
+			}
+
+			@Override
 			public boolean belongsTo(Object family) {
 				if (JobFamilies.FETCH.equals(family))
 					return true;
 				return super.belongsTo(family);
 			}
+
 		};
 		job.setUser(true);
 		job.schedule();
-		job.addJobChangeListener(new JobChangeAdapter() {
-			@Override
-			public void done(IJobChangeEvent event) {
-				if (event.getResult().isOK())
-					FetchResultDialog.show(repository, op.getOperationResult(),
-							sourceString);
-				else
-					Activator.handleError(event.getResult().getMessage(), event
-							.getResult().getException(), true);
-			}
-		});
 	}
 
 	/**
@@ -156,4 +161,28 @@ public class FetchOperationUI {
 	public String getSourceString(){
 		return sourceString;
 	}
+
+	private static class ShowResultAction extends RepositoryJobResultAction {
+
+		private final FetchResult result;
+
+		private final String source;
+
+		public ShowResultAction(@NonNull Repository repository,
+				FetchResult result, String source) {
+			super(repository, UIText.FetchOperationUI_ShowFetchResult);
+			this.result = result;
+			this.source = source;
+		}
+
+		@Override
+		protected void showResult(@NonNull Repository repository) {
+			Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+					.getShell();
+			FetchResultDialog dialog = new FetchResultDialog(shell, repository,
+					result, source);
+			dialog.open();
+		}
+	}
+
 }

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushOperationUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 SAP AG and others.
+ * Copyright (c) 2011, 2016 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Mathias Kinzler (SAP AG) - initial implementation
+ *    Thomas Wolf <thomas.wolf@paranor.ch> - Bug 495512
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.push;
 
@@ -20,9 +21,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.egit.core.op.PushOperation;
 import org.eclipse.egit.core.op.PushOperationResult;
 import org.eclipse.egit.core.op.PushOperationSpecification;
@@ -31,6 +30,8 @@ import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.credentials.EGitCredentialsProvider;
+import org.eclipse.egit.ui.internal.jobs.RepositoryJob;
+import org.eclipse.jface.action.Action;
 import org.eclipse.jgit.errors.NotSupportedException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.CredentialsProvider;
@@ -221,12 +222,19 @@ public class PushOperationUI {
 	 * completion
 	 */
 	public void start() {
-		Job job = new Job(NLS.bind(UIText.PushOperationUI_PushJobName,
+		final Repository repo = repository;
+		if (repo == null) {
+			return;
+		}
+		Job job = new RepositoryJob(NLS.bind(UIText.PushOperationUI_PushJobName,
 				destinationString)) {
+
+			private PushOperationResult result;
+
 			@Override
-			protected IStatus run(IProgressMonitor monitor) {
+			protected IStatus performJob(IProgressMonitor monitor) {
 				try {
-					execute(monitor);
+					result = execute(monitor);
 				} catch (CoreException e) {
 					return Activator.createErrorStatus(e.getStatus()
 							.getMessage(), e);
@@ -235,28 +243,24 @@ public class PushOperationUI {
 			}
 
 			@Override
+			protected Action getAction() {
+				if (expectedResult == null || !expectedResult.equals(result)) {
+					return new ShowPushResultAction(repo, result,
+							destinationString, showConfigureButton);
+				}
+				return null;
+			}
+
+			@Override
 			public boolean belongsTo(Object family) {
 				if (JobFamilies.PUSH.equals(family))
 					return true;
 				return super.belongsTo(family);
 			}
+
 		};
 		job.setUser(true);
 		job.schedule();
-		job.addJobChangeListener(new JobChangeAdapter() {
-			@Override
-			public void done(IJobChangeEvent event) {
-				PushOperationResult result = op.getOperationResult();
-				if (expectedResult == null || !expectedResult.equals(result)) {
-					if (event.getResult().isOK())
-						PushResultDialog.show(repository, result,
-								destinationString, showConfigureButton, false);
-					else
-						Activator.handleError(event.getResult().getMessage(),
-								event.getResult().getException(), true);
-				}
-			}
-		});
 	}
 
 	/**
@@ -270,4 +274,5 @@ public class PushOperationUI {
 		return Activator.getDefault().getPreferenceStore()
 				.getInt(UIPreferences.REMOTE_CONNECTION_TIMEOUT);
 	}
+
 }

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchOperationUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 SAP AG.
+ * Copyright (c) 2011, 2016 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,25 +7,27 @@
  *
  * Contributors:
  *    Mathias Kinzler (SAP AG) - initial implementation
+ *    Thomas Wolf <thomas.wolf@paranor.ch> - Bug 495512
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.fetch;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.egit.core.op.FetchOperation;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.credentials.EGitCredentialsProvider;
+import org.eclipse.egit.ui.internal.jobs.RepositoryJob;
+import org.eclipse.egit.ui.internal.jobs.RepositoryJobResultAction;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jgit.annotations.NonNull;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.FetchResult;
@@ -34,6 +36,8 @@ import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.TagOpt;
 import org.eclipse.jgit.transport.URIish;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * UI Wrapper for {@link FetchOperation}
@@ -113,14 +117,20 @@ public class FetchOperationUI {
 	 * completion
 	 */
 	public void start() {
-		Job job = new WorkspaceJob(NLS.bind(
+		final Repository repo = repository;
+		if (repo == null) {
+			return;
+		}
+		Job job = new RepositoryJob(NLS.bind(
 				UIText.FetchOperationUI_FetchJobName,
 				sourceString)) {
 
+			private FetchResult result;
+
 			@Override
-			public IStatus runInWorkspace(IProgressMonitor monitor) {
+			public IStatus performJob(IProgressMonitor monitor) {
 				try {
-					execute(monitor);
+					result = execute(monitor);
 				} catch (CoreException e) {
 					return Activator.createErrorStatus(e.getStatus()
 							.getMessage(), e);
@@ -129,25 +139,21 @@ public class FetchOperationUI {
 			}
 
 			@Override
+			protected IAction getAction() {
+				return new ShowResultAction(repo, result, sourceString);
+			}
+
+			@Override
 			public boolean belongsTo(Object family) {
-				if (JobFamilies.FETCH.equals(family))
+				if (JobFamilies.FETCH.equals(family)) {
 					return true;
+				}
 				return super.belongsTo(family);
 			}
+
 		};
 		job.setUser(true);
 		job.schedule();
-		job.addJobChangeListener(new JobChangeAdapter() {
-			@Override
-			public void done(IJobChangeEvent event) {
-				if (event.getResult().isOK())
-					FetchResultDialog.show(repository, op.getOperationResult(),
-							sourceString);
-				else
-					Activator.handleError(event.getResult().getMessage(), event
-							.getResult().getException(), true);
-			}
-		});
 	}
 
 	/**
@@ -156,4 +162,28 @@ public class FetchOperationUI {
 	public String getSourceString(){
 		return sourceString;
 	}
+
+	private static class ShowResultAction extends RepositoryJobResultAction {
+
+		private final FetchResult result;
+
+		private final String source;
+
+		public ShowResultAction(@NonNull Repository repository,
+				FetchResult result, String source) {
+			super(repository, UIText.FetchOperationUI_ShowFetchResult);
+			this.result = result;
+			this.source = source;
+		}
+
+		@Override
+		protected void showResult(@NonNull Repository repository) {
+			Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+					.getShell();
+			FetchResultDialog dialog = new FetchResultDialog(shell, repository,
+					result, source);
+			dialog.open();
+		}
+	}
+
 }

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushOperationUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 SAP AG and others.
+ * Copyright (c) 2011, 2016 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Mathias Kinzler (SAP AG) - initial implementation
+ *    Thomas Wolf <thomas.wolf@paranor.ch> - Bug 495512
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.push;
 
@@ -20,9 +21,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.egit.core.op.PushOperation;
 import org.eclipse.egit.core.op.PushOperationResult;
 import org.eclipse.egit.core.op.PushOperationSpecification;
@@ -31,6 +30,8 @@ import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.credentials.EGitCredentialsProvider;
+import org.eclipse.egit.ui.internal.jobs.RepositoryJob;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jgit.errors.NotSupportedException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.CredentialsProvider;
@@ -221,12 +222,19 @@ public class PushOperationUI {
 	 * completion
 	 */
 	public void start() {
-		Job job = new Job(NLS.bind(UIText.PushOperationUI_PushJobName,
+		final Repository repo = repository;
+		if (repo == null) {
+			return;
+		}
+		Job job = new RepositoryJob(NLS.bind(UIText.PushOperationUI_PushJobName,
 				destinationString)) {
+
+			private PushOperationResult result;
+
 			@Override
-			protected IStatus run(IProgressMonitor monitor) {
+			protected IStatus performJob(IProgressMonitor monitor) {
 				try {
-					execute(monitor);
+					result = execute(monitor);
 				} catch (CoreException e) {
 					return Activator.createErrorStatus(e.getStatus()
 							.getMessage(), e);
@@ -235,28 +243,25 @@ public class PushOperationUI {
 			}
 
 			@Override
+			protected IAction getAction() {
+				if (expectedResult == null || !expectedResult.equals(result)) {
+					return new ShowPushResultAction(repo, result,
+							destinationString, showConfigureButton);
+				}
+				return null;
+			}
+
+			@Override
 			public boolean belongsTo(Object family) {
-				if (JobFamilies.PUSH.equals(family))
+				if (JobFamilies.PUSH.equals(family)) {
 					return true;
+				}
 				return super.belongsTo(family);
 			}
+
 		};
 		job.setUser(true);
 		job.schedule();
-		job.addJobChangeListener(new JobChangeAdapter() {
-			@Override
-			public void done(IJobChangeEvent event) {
-				PushOperationResult result = op.getOperationResult();
-				if (expectedResult == null || !expectedResult.equals(result)) {
-					if (event.getResult().isOK())
-						PushResultDialog.show(repository, result,
-								destinationString, showConfigureButton, false);
-					else
-						Activator.handleError(event.getResult().getMessage(),
-								event.getResult().getException(), true);
-				}
-			}
-		});
 	}
 
 	/**
@@ -270,4 +275,5 @@ public class PushOperationUI {
 		return Activator.getDefault().getPreferenceStore()
 				.getInt(UIPreferences.REMOTE_CONNECTION_TIMEOUT);
 	}
+
 }