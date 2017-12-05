eclipse-egit/egit

Specialized labels for Push dialog buttons
Bug: 522118
Change-Id: Ib7cd6c44f4e0b863f2111104ed99e99e67908b9d
Signed-off-by: Mickael Istria <mistria@redhat.com>
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/PushBranchActionHandler.java
@@ -12,8 +12,8 @@ import java.io.IOException;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.push.PushBranchDialog;
 import org.eclipse.egit.ui.internal.push.PushBranchWizard;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
@@ -36,7 +36,8 @@ public class PushBranchActionHandler extends RepositoryActionHandler {
 				ObjectId id = repository.resolve(repository.getFullBranch());
 				wizard = new PushBranchWizard(repository, id);
 			}
-			WizardDialog dlg = new WizardDialog(getShell(event), wizard);
+			PushBranchDialog dlg = new PushBranchDialog(getShell(event),
+					wizard);
 			dlg.open();
 		} catch (IOException ex) {
 			Activator.handleError(ex.getLocalizedMessage(), ex, false);

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/PushUpstreamOrBranchActionHandler.java
@@ -12,10 +12,10 @@ import java.io.IOException;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.ui.internal.push.PushBranchDialog;
 import org.eclipse.egit.ui.internal.push.PushBranchWizard;
 import org.eclipse.egit.ui.internal.push.PushOperationUI;
 import org.eclipse.egit.ui.internal.push.SimpleConfigurePushDialog;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
@@ -56,7 +56,7 @@ public class PushUpstreamOrBranchActionHandler extends RepositoryActionHandler {
 				PushBranchWizard pushBranchWizard = new PushBranchWizard(
 						repository, head);
 
-				WizardDialog dlg = new WizardDialog(shell,
+				PushBranchDialog dlg = new PushBranchDialog(shell,
 						pushBranchWizard);
 				dlg.open();
 			}

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/PushCommitHandler.java
@@ -12,9 +12,9 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.history.GitHistoryPage;
+import org.eclipse.egit.ui.internal.push.PushBranchDialog;
 import org.eclipse.egit.ui.internal.push.PushBranchWizard;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
@@ -44,7 +44,7 @@ public class PushCommitHandler extends AbstractHistoryCommandHandler {
 				wizard = new PushBranchWizard(repo, commit.getId());
 			else
 				wizard = new PushBranchWizard(repo, localBranch);
-			WizardDialog dlg = new WizardDialog(
+			PushBranchDialog dlg = new PushBranchDialog(
 					HandlerUtil.getActiveShellChecked(event),
 					wizard);
 			dlg.setHelpAvailable(true);

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/PushBranchActionHandler.java
@@ -12,8 +12,8 @@ import java.io.IOException;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.push.PushWizardDialog;
 import org.eclipse.egit.ui.internal.push.PushBranchWizard;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
@@ -36,7 +36,8 @@ public class PushBranchActionHandler extends RepositoryActionHandler {
 				ObjectId id = repository.resolve(repository.getFullBranch());
 				wizard = new PushBranchWizard(repository, id);
 			}
-			WizardDialog dlg = new WizardDialog(getShell(event), wizard);
+			PushWizardDialog dlg = new PushWizardDialog(getShell(event),
+					wizard);
 			dlg.open();
 		} catch (IOException ex) {
 			Activator.handleError(ex.getLocalizedMessage(), ex, false);

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/PushUpstreamOrBranchActionHandler.java
@@ -12,10 +12,10 @@ import java.io.IOException;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.ui.internal.push.PushWizardDialog;
 import org.eclipse.egit.ui.internal.push.PushBranchWizard;
 import org.eclipse.egit.ui.internal.push.PushOperationUI;
 import org.eclipse.egit.ui.internal.push.SimpleConfigurePushDialog;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
@@ -56,7 +56,7 @@ public class PushUpstreamOrBranchActionHandler extends RepositoryActionHandler {
 				PushBranchWizard pushBranchWizard = new PushBranchWizard(
 						repository, head);
 
-				WizardDialog dlg = new WizardDialog(shell,
+				PushWizardDialog dlg = new PushWizardDialog(shell,
 						pushBranchWizard);
 				dlg.open();
 			}

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/PushCommitHandler.java
@@ -12,9 +12,9 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.history.GitHistoryPage;
+import org.eclipse.egit.ui.internal.push.PushWizardDialog;
 import org.eclipse.egit.ui.internal.push.PushBranchWizard;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
@@ -44,7 +44,7 @@ public class PushCommitHandler extends AbstractHistoryCommandHandler {
 				wizard = new PushBranchWizard(repo, commit.getId());
 			else
 				wizard = new PushBranchWizard(repo, localBranch);
-			WizardDialog dlg = new WizardDialog(
+			PushWizardDialog dlg = new PushWizardDialog(
 					HandlerUtil.getActiveShellChecked(event),
 					wizard);
 			dlg.setHelpAvailable(true);