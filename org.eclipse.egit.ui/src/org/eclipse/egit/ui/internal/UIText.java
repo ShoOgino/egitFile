eclipse-egit/egit

Use verbs in BranchSelectionDialog
Instead of OK, show the verb (checkout, delete, ...) of the action to be
done after the branch has been selected.

The verbs have accelerators and those are the same like at other places
in there UI, where applicable.

Bug: 533708
Change-Id: I4ca57d09898e1e066329b81ae8c3ecf4b0a6a7f9
Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -1196,6 +1196,9 @@ public class UIText extends NLS {
 	public static String CheckoutDialog_Title;
 
 	/** */
+	public static String CheckoutHandler_SelectBranchButton;
+
+	/** */
 	public static String CheckoutHandler_SelectBranchMessage;
 
 	/** */
@@ -1311,6 +1314,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CompareUtils_wrongResourceArgument;
+
+	/** */
+	public static String MergeHandler_SelectBranchButton;
 
 	/** */
 	public static String MergeHandler_SelectBranchMessage;
@@ -3503,6 +3509,9 @@ public class UIText extends NLS {
 	public static String DeleteBranchDialog_WindowTitle;
 
 	/** */
+	public static String DeleteBranchOnCommitHandler_SelectBranchDialogButton;
+
+	/** */
 	public static String DeleteBranchOnCommitHandler_SelectBranchDialogMessage;
 
 	/** */
@@ -3530,6 +3539,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String DeleteRepositoryConfirmDialog_DeleteProjectsCheckbox;
+
+	/** */
+	public static String DeleteTagOnCommitHandler_SelectTagDialogButton;
 
 	/** */
 	public static String DeleteTagOnCommitHandler_SelectTagDialogMessage;
@@ -4759,6 +4771,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String RenameBranchDialog_WindowTitle;
+
+	/** */
+	public static String RenameBranchOnCommitHandler_SelectBranchDialogButton;
 
 	/** */
 	public static String RenameBranchOnCommitHandler_SelectBranchDialogMessage;

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CheckoutCommitHandler.java
@@ -53,7 +53,8 @@ public class CheckoutCommitHandler extends AbstractHistoryCommandHandler {
 			BranchSelectionDialog<Ref> dlg = new BranchSelectionDialog<>(
 					HandlerUtil.getActiveShellChecked(event), nodes,
 					UIText.CheckoutHandler_SelectBranchTitle,
-					UIText.CheckoutHandler_SelectBranchMessage, SWT.SINGLE);
+					UIText.CheckoutHandler_SelectBranchMessage,
+					UIText.CheckoutHandler_SelectBranchButton, SWT.SINGLE);
 			if (dlg.open() == Window.OK) {
 				op = BranchOperationUI.checkout(repo,
 						dlg.getSelectedNode().getName());

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/DeleteBranchOnCommitHandler.java
@@ -75,6 +75,7 @@ public class DeleteBranchOnCommitHandler extends AbstractHistoryCommandHandler {
 					branchesOfCommit,
 					UIText.DeleteBranchOnCommitHandler_SelectBranchDialogTitle,
 					UIText.DeleteBranchOnCommitHandler_SelectBranchDialogMessage,
+					UIText.DeleteBranchOnCommitHandler_SelectBranchDialogButton,
 					SWT.MULTI);
 			if (dlg.open() != Window.OK)
 				return null;

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/DeleteTagOnCommitHandler.java
@@ -62,6 +62,7 @@ public class DeleteTagOnCommitHandler extends AbstractHistoryCommandHandler {
 					tags,
 					UIText.DeleteTagOnCommitHandler_SelectTagDialogTitle,
 					UIText.DeleteTagOnCommitHandler_SelectTagDialogMessage,
+					UIText.DeleteTagOnCommitHandler_SelectTagDialogButton,
 					SWT.MULTI);
 			if (dialog.open() != Window.OK)
 				return null;

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/MergeHandler.java
@@ -71,7 +71,8 @@ public class MergeHandler extends AbstractHistoryCommandHandler {
 			BranchSelectionDialog<Ref> dlg = new BranchSelectionDialog<>(
 					shell, nodes,
 					UIText.MergeHandler_SelectBranchTitle,
-					UIText.MergeHandler_SelectBranchMessage, SWT.SINGLE);
+					UIText.MergeHandler_SelectBranchMessage,
+					UIText.MergeHandler_SelectBranchButton, SWT.SINGLE);
 			if (dlg.open() == Window.OK) {
 				refName = dlg.getSelectedNode().getName();
 			} else {

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/RenameBranchOnCommitHandler.java
@@ -46,6 +46,7 @@ public class RenameBranchOnCommitHandler extends AbstractHistoryCommandHandler {
 					branchesOfCommit,
 					UIText.RenameBranchOnCommitHandler_SelectBranchDialogTitle,
 					UIText.RenameBranchOnCommitHandler_SelectBranchDialogMessage,
+					UIText.RenameBranchOnCommitHandler_SelectBranchDialogButton,
 					SWT.SINGLE);
 			if (dlg.open() != Window.OK)
 				return null;

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -1196,10 +1196,13 @@ public class UIText extends NLS {
 	public static String CheckoutDialog_Title;
 
 	/** */
-	public static String CheckoutHandler_SelectBranchMessage;
+	public static String CheckoutHandler_CheckoutBranchDialogButton;
 
 	/** */
-	public static String CheckoutHandler_SelectBranchTitle;
+	public static String CheckoutHandler_CheckoutBranchDialogMessage;
+
+	/** */
+	public static String CheckoutHandler_CheckoutBranchDialogTitle;
 
 	/** */
 	public static String CherryPickHandler_NoCherryPickPerformedMessage;
@@ -1313,10 +1316,13 @@ public class UIText extends NLS {
 	public static String CompareUtils_wrongResourceArgument;
 
 	/** */
-	public static String MergeHandler_SelectBranchMessage;
+	public static String MergeHandler_MergeBranchDialogButton;
 
 	/** */
-	public static String MergeHandler_SelectBranchTitle;
+	public static String MergeHandler_MergeBranchDialogMessage;
+
+	/** */
+	public static String MergeHandler_MergeBranchDialogTitle;
 
 	/** */
 	public static String RepositoryAction_errorFindingRepo;
@@ -3503,10 +3509,13 @@ public class UIText extends NLS {
 	public static String DeleteBranchDialog_WindowTitle;
 
 	/** */
-	public static String DeleteBranchOnCommitHandler_SelectBranchDialogMessage;
+	public static String DeleteBranchOnCommitHandler_DeleteBranchesDialogButton;
 
 	/** */
-	public static String DeleteBranchOnCommitHandler_SelectBranchDialogTitle;
+	public static String DeleteBranchOnCommitHandler_DeleteBranchesDialogMessage;
+
+	/** */
+	public static String DeleteBranchOnCommitHandler_DeleteBranchesDialogTitle;
 
 	/** */
 	public static String DeleteRepositoryConfirmDialog_DeleteGitDirCheckbox;
@@ -3532,10 +3541,13 @@ public class UIText extends NLS {
 	public static String DeleteRepositoryConfirmDialog_DeleteProjectsCheckbox;
 
 	/** */
-	public static String DeleteTagOnCommitHandler_SelectTagDialogMessage;
+	public static String DeleteTagOnCommitHandler_DeleteTagsDialogButton;
 
 	/** */
-	public static String DeleteTagOnCommitHandler_SelectTagDialogTitle;
+	public static String DeleteTagOnCommitHandler_DeleteTagsDialogMessage;
+
+	/** */
+	public static String DeleteTagOnCommitHandler_DeleteTagsDialogTitle;
 
 	/** */
 	public static String DeleteTagCommand_deletingTagsProgress;
@@ -4764,10 +4776,13 @@ public class UIText extends NLS {
 	public static String RenameBranchDialog_WindowTitle;
 
 	/** */
-	public static String RenameBranchOnCommitHandler_SelectBranchDialogMessage;
+	public static String RenameBranchOnCommitHandler_RenameBranchDialogButton;
 
 	/** */
-	public static String RenameBranchOnCommitHandler_SelectBranchDialogTitle;
+	public static String RenameBranchOnCommitHandler_RenameBranchDialogMessage;
+
+	/** */
+	public static String RenameBranchOnCommitHandler_RenameBranchDialogTitle;
 
 	/** */
 	public static String RepositoryStatistics_Description;

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CheckoutCommitHandler.java
@@ -52,8 +52,9 @@ public class CheckoutCommitHandler extends AbstractHistoryCommandHandler {
 		} else {
 			BranchSelectionDialog<Ref> dlg = new BranchSelectionDialog<>(
 					HandlerUtil.getActiveShellChecked(event), nodes,
-					UIText.CheckoutHandler_SelectBranchTitle,
-					UIText.CheckoutHandler_SelectBranchMessage, SWT.SINGLE);
+					UIText.CheckoutHandler_CheckoutBranchDialogTitle,
+					UIText.CheckoutHandler_CheckoutBranchDialogMessage,
+					UIText.CheckoutHandler_CheckoutBranchDialogButton, SWT.SINGLE);
 			if (dlg.open() == Window.OK) {
 				op = BranchOperationUI.checkout(repo,
 						dlg.getSelectedNode().getName());

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/DeleteBranchOnCommitHandler.java
@@ -73,8 +73,9 @@ public class DeleteBranchOnCommitHandler extends AbstractHistoryCommandHandler {
 			BranchSelectionDialog<Ref> dlg = new BranchSelectionDialog<>(
 					shell,
 					branchesOfCommit,
-					UIText.DeleteBranchOnCommitHandler_SelectBranchDialogTitle,
-					UIText.DeleteBranchOnCommitHandler_SelectBranchDialogMessage,
+					UIText.DeleteBranchOnCommitHandler_DeleteBranchesDialogTitle,
+					UIText.DeleteBranchOnCommitHandler_DeleteBranchesDialogMessage,
+					UIText.DeleteBranchOnCommitHandler_DeleteBranchesDialogButton,
 					SWT.MULTI);
 			if (dlg.open() != Window.OK)
 				return null;

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/DeleteTagOnCommitHandler.java
@@ -60,8 +60,9 @@ public class DeleteTagOnCommitHandler extends AbstractHistoryCommandHandler {
 			BranchSelectionDialog<Ref> dialog = new BranchSelectionDialog<>(
 					shell,
 					tags,
-					UIText.DeleteTagOnCommitHandler_SelectTagDialogTitle,
-					UIText.DeleteTagOnCommitHandler_SelectTagDialogMessage,
+					UIText.DeleteTagOnCommitHandler_DeleteTagsDialogTitle,
+					UIText.DeleteTagOnCommitHandler_DeleteTagsDialogMessage,
+					UIText.DeleteTagOnCommitHandler_DeleteTagsDialogButton,
 					SWT.MULTI);
 			if (dialog.open() != Window.OK)
 				return null;

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/MergeHandler.java
@@ -70,8 +70,9 @@ public class MergeHandler extends AbstractHistoryCommandHandler {
 		} else {
 			BranchSelectionDialog<Ref> dlg = new BranchSelectionDialog<>(
 					shell, nodes,
-					UIText.MergeHandler_SelectBranchTitle,
-					UIText.MergeHandler_SelectBranchMessage, SWT.SINGLE);
+					UIText.MergeHandler_MergeBranchDialogTitle,
+					UIText.MergeHandler_MergeBranchDialogMessage,
+					UIText.MergeHandler_MergeBranchDialogButton, SWT.SINGLE);
 			if (dlg.open() == Window.OK) {
 				refName = dlg.getSelectedNode().getName();
 			} else {

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/RenameBranchOnCommitHandler.java
@@ -44,8 +44,9 @@ public class RenameBranchOnCommitHandler extends AbstractHistoryCommandHandler {
 			BranchSelectionDialog<Ref> dlg = new BranchSelectionDialog<>(
 					shell,
 					branchesOfCommit,
-					UIText.RenameBranchOnCommitHandler_SelectBranchDialogTitle,
-					UIText.RenameBranchOnCommitHandler_SelectBranchDialogMessage,
+					UIText.RenameBranchOnCommitHandler_RenameBranchDialogTitle,
+					UIText.RenameBranchOnCommitHandler_RenameBranchDialogMessage,
+					UIText.RenameBranchOnCommitHandler_RenameBranchDialogButton,
 					SWT.SINGLE);
 			if (dlg.open() != Window.OK)
 				return null;