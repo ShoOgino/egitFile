eclipse-egit/egit

Rename RepositoriesViewLabelProvider to RepositoryTreeNodeLabelProvider
It's not a label provider for the git repositories view; it is
a label provider for RepositoryTreeNodes. (Which happen to be
_also_ shown in the git repositories view, but they're also
shown and labeled through this class elsewhere.)

Change-Id: I5221ba06aaf475b3b577b8ccb2846b62a64331c8
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitSelectRepositoryPage.java
@@ -29,7 +29,7 @@ import org.eclipse.egit.core.internal.gerrit.GerritUtil;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.repository.RepositoriesViewContentProvider;
-import org.eclipse.egit.ui.internal.repository.RepositoriesViewStyledCellLabelProvider;
+import org.eclipse.egit.ui.internal.repository.RepositoryTreeNodeStyledCellLabelProvider;
 import org.eclipse.egit.ui.internal.repository.RepositorySearchWizard;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
@@ -161,7 +161,7 @@ public class GitSelectRepositoryPage extends WizardPage {
 				return false;
 			}
 		});
-		tv.setLabelProvider(new RepositoriesViewStyledCellLabelProvider());
+		tv.setLabelProvider(new RepositoryTreeNodeStyledCellLabelProvider());
 
 		Composite tb = new Composite(main, SWT.NONE);
 		GridLayoutFactory.fillDefaults().numColumns(1).applyTo(tb);

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitSelectWizardPage.java
@@ -23,7 +23,7 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.repository.RepositoriesViewContentProvider;
-import org.eclipse.egit.ui.internal.repository.RepositoriesViewStyledCellLabelProvider;
+import org.eclipse.egit.ui.internal.repository.RepositoryTreeNodeStyledCellLabelProvider;
 import org.eclipse.egit.ui.internal.repository.tree.FolderNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNodeType;
@@ -200,7 +200,7 @@ public class GitSelectWizardPage extends WizardPage {
 		tv.setContentProvider(cp);
 		GridDataFactory.fillDefaults().grab(true, true).hint(SWT.DEFAULT, 200)
 				.applyTo(tv.getTree());
-		tv.setLabelProvider(new RepositoriesViewStyledCellLabelProvider());
+		tv.setLabelProvider(new RepositoryTreeNodeStyledCellLabelProvider());
 
 		tv.addSelectionChangedListener(new ISelectionChangedListener() {
 

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/search/CommitSearchPage.java
@@ -28,7 +28,7 @@ import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.internal.UIIcons;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.repository.RepositoriesViewContentProvider;
-import org.eclipse.egit.ui.internal.repository.RepositoriesViewStyledCellLabelProvider;
+import org.eclipse.egit.ui.internal.repository.RepositoryTreeNodeStyledCellLabelProvider;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.DialogPage;
@@ -312,7 +312,7 @@ public class CommitSearchPage extends DialogPage implements ISearchPage {
 				repositoryGroup, SWT.SINGLE | SWT.V_SCROLL | SWT.H_SCROLL
 						| SWT.BORDER);
 		this.repositoryViewer
-				.setLabelProvider(new RepositoriesViewStyledCellLabelProvider());
+				.setLabelProvider(new RepositoryTreeNodeStyledCellLabelProvider());
 		this.repositoryViewer
 				.setContentProvider(new RepositoriesViewContentProvider());
 		this.repositoryViewer

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitSelectRepositoryPage.java
@@ -29,7 +29,7 @@ import org.eclipse.egit.core.internal.gerrit.GerritUtil;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.repository.RepositoriesViewContentProvider;
-import org.eclipse.egit.ui.internal.repository.RepositoriesViewLabelProvider;
+import org.eclipse.egit.ui.internal.repository.RepositoryTreeNodeLabelProvider;
 import org.eclipse.egit.ui.internal.repository.RepositorySearchWizard;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
@@ -161,7 +161,7 @@ public class GitSelectRepositoryPage extends WizardPage {
 				return false;
 			}
 		});
-		tv.setLabelProvider(new RepositoriesViewLabelProvider());
+		tv.setLabelProvider(new RepositoryTreeNodeLabelProvider());
 
 		Composite tb = new Composite(main, SWT.NONE);
 		GridLayoutFactory.fillDefaults().numColumns(1).applyTo(tb);

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitSelectWizardPage.java
@@ -23,7 +23,7 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.repository.RepositoriesViewContentProvider;
-import org.eclipse.egit.ui.internal.repository.RepositoriesViewLabelProvider;
+import org.eclipse.egit.ui.internal.repository.RepositoryTreeNodeLabelProvider;
 import org.eclipse.egit.ui.internal.repository.tree.FolderNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNodeType;
@@ -200,8 +200,7 @@ public class GitSelectWizardPage extends WizardPage {
 		tv.setContentProvider(cp);
 		GridDataFactory.fillDefaults().grab(true, true).hint(SWT.DEFAULT, 200)
 				.applyTo(tv.getTree());
-		tv.setLabelProvider(new RepositoriesViewLabelProvider());
-
+		tv.setLabelProvider(new RepositoryTreeNodeLabelProvider());
 		tv.addSelectionChangedListener(new ISelectionChangedListener() {
 
 			@Override

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/search/CommitSearchPage.java
@@ -28,7 +28,7 @@ import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.internal.UIIcons;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.repository.RepositoriesViewContentProvider;
-import org.eclipse.egit.ui.internal.repository.RepositoriesViewLabelProvider;
+import org.eclipse.egit.ui.internal.repository.RepositoryTreeNodeLabelProvider;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.DialogPage;
@@ -312,7 +312,7 @@ public class CommitSearchPage extends DialogPage implements ISearchPage {
 				repositoryGroup, SWT.SINGLE | SWT.V_SCROLL | SWT.H_SCROLL
 						| SWT.BORDER);
 		this.repositoryViewer
-				.setLabelProvider(new RepositoriesViewLabelProvider());
+				.setLabelProvider(new RepositoryTreeNodeLabelProvider());
 		this.repositoryViewer
 				.setContentProvider(new RepositoriesViewContentProvider());
 		this.repositoryViewer