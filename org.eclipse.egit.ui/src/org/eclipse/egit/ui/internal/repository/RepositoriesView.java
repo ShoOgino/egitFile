eclipse-egit/egit

Refactor getNodeForPath to accept different node types
Change-Id: I78e344d7782d5470c62bd2ef6979c22104d4b112
Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesView.java
@@ -989,8 +989,8 @@ public class RepositoriesView extends CommonNavigator implements IShowInSource, 
 		}
 	}
 
-	private RepositoryTreeNode getNodeForPath(Repository repository, String repoRelativePath) {
-		RepositoryTreeNode currentNode = null;
+	private RepositoryTreeNode getRepositoryChildNode(Repository repository,
+			RepositoryTreeNodeType type) {
 		ITreeContentProvider cp = (ITreeContentProvider) getCommonViewer()
 				.getContentProvider();
 		for (Object repo : cp.getElements(getCommonViewer().getInput())) {
@@ -1000,19 +1000,27 @@ public class RepositoriesView extends CommonNavigator implements IShowInSource, 
 					((Repository) node.getObject()).getDirectory())) {
 				for (Object child : cp.getChildren(node)) {
 					RepositoryTreeNode childNode = (RepositoryTreeNode) child;
-					if (childNode.getType() == RepositoryTreeNodeType.WORKINGDIR) {
-						currentNode = childNode;
-						break;
+					if (childNode.getType() == type) {
+						return childNode;
 					}
 				}
-				break;
 			}
 		}
+		return null;
+	}
 
+	private RepositoryTreeNode getNodeForPath(Repository repository,
+			String repoRelativePath) {
+		RepositoryTreeNode workingDirNode = getRepositoryChildNode(repository,
+				RepositoryTreeNodeType.WORKINGDIR);
+		RepositoryTreeNode currentNode = workingDirNode;
+
+		ITreeContentProvider cp = (ITreeContentProvider) getCommonViewer()
+				.getContentProvider();
 		IPath relPath = new Path(repoRelativePath);
 
-		for (String segment : relPath.segments())
-			for (Object child : cp.getChildren(currentNode)) {
+		for (String segment : relPath.segments()) {
+			for (Object child : cp.getChildren(workingDirNode)) {
 				@SuppressWarnings("unchecked")
 				RepositoryTreeNode<File> childNode = (RepositoryTreeNode<File>) child;
 				if (childNode.getObject().getName().equals(segment)) {
@@ -1020,7 +1028,7 @@ public class RepositoriesView extends CommonNavigator implements IShowInSource, 
 					break;
 				}
 			}
-
+		}
 		return currentNode;
 	}
 

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesView.java
@@ -989,8 +989,8 @@ public class RepositoriesView extends CommonNavigator implements IShowInSource, 
 		}
 	}
 
-	private RepositoryTreeNode getNodeForPath(Repository repository, String repoRelativePath) {
-		RepositoryTreeNode currentNode = null;
+	private RepositoryTreeNode getRepositoryChildNode(Repository repository,
+			RepositoryTreeNodeType type) {
 		ITreeContentProvider cp = (ITreeContentProvider) getCommonViewer()
 				.getContentProvider();
 		for (Object repo : cp.getElements(getCommonViewer().getInput())) {
@@ -1000,18 +1000,25 @@ public class RepositoriesView extends CommonNavigator implements IShowInSource, 
 					((Repository) node.getObject()).getDirectory())) {
 				for (Object child : cp.getChildren(node)) {
 					RepositoryTreeNode childNode = (RepositoryTreeNode) child;
-					if (childNode.getType() == RepositoryTreeNodeType.WORKINGDIR) {
-						currentNode = childNode;
-						break;
+					if (childNode.getType() == type) {
+						return childNode;
 					}
 				}
-				break;
 			}
 		}
+		return null;
+	}
 
+	private RepositoryTreeNode getNodeForPath(Repository repository,
+			String repoRelativePath) {
+		RepositoryTreeNode currentNode = getRepositoryChildNode(repository,
+				RepositoryTreeNodeType.WORKINGDIR);
+
+		ITreeContentProvider cp = (ITreeContentProvider) getCommonViewer()
+				.getContentProvider();
 		IPath relPath = new Path(repoRelativePath);
 
-		for (String segment : relPath.segments())
+		for (String segment : relPath.segments()) {
 			for (Object child : cp.getChildren(currentNode)) {
 				@SuppressWarnings("unchecked")
 				RepositoryTreeNode<File> childNode = (RepositoryTreeNode<File>) child;
@@ -1020,7 +1027,7 @@ public class RepositoriesView extends CommonNavigator implements IShowInSource, 
 					break;
 				}
 			}
-
+		}
 		return currentNode;
 	}
 