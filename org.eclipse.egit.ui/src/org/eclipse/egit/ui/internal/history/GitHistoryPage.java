eclipse-egit/egit

Suppress history for structural RepositoryTreeNodes
Don't adapt the structural tree nodes of the repository view to history
input. This avoids unwantedly showing the full repository history when
clicking on any of those nodes just for expanding/navigating the tree,
therefore not loosing the history of the previously selected
branch/tag/ref.

Bug:549034
Change-Id: I7761b29f6a33d9fa7b992ec296afba8a60016a70
Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -23,6 +23,7 @@ package org.eclipse.egit.ui.internal.history;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -66,6 +67,7 @@ import org.eclipse.egit.ui.internal.repository.tree.FolderNode;
 import org.eclipse.egit.ui.internal.repository.tree.RefNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNodeType;
 import org.eclipse.egit.ui.internal.repository.tree.TagNode;
 import org.eclipse.egit.ui.internal.selection.RepositorySelectionProvider;
 import org.eclipse.egit.ui.internal.selection.SelectionUtils;
@@ -878,22 +880,42 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 	 *         FOLDER or PROJECT and we can show it; false otherwise.
 	 */
 	public static boolean canShowHistoryFor(final Object object) {
-		if (object instanceof HistoryPageInput)
+		if (object instanceof HistoryPageInput) {
 			return true;
+		}
 
-		if (object instanceof IResource)
+		if (object instanceof IResource) {
 			return typeOk((IResource) object);
+		}
 
-		if (object instanceof RepositoryTreeNode)
-			return true;
+		if (object instanceof RepositoryTreeNode) {
+			// ignore structural nodes from the repo view
+			RepositoryTreeNodeType nodeType = ((RepositoryTreeNode) object)
+					.getType();
+			boolean unsupportedNodeType = EnumSet.of(
+					RepositoryTreeNodeType.ADDITIONALREFS,
+					RepositoryTreeNodeType.BRANCHES,
+					RepositoryTreeNodeType.BRANCHHIERARCHY,
+					RepositoryTreeNodeType.ERROR, RepositoryTreeNodeType.FETCH,
+					RepositoryTreeNodeType.LOCAL, RepositoryTreeNodeType.PUSH,
+					RepositoryTreeNodeType.REMOTE,
+					RepositoryTreeNodeType.REMOTES,
+					RepositoryTreeNodeType.REMOTETRACKING,
+					RepositoryTreeNodeType.STASH,
+					RepositoryTreeNodeType.STASHED_COMMIT,
+					RepositoryTreeNodeType.TAGS)
+					.contains(nodeType);
+			return !unsupportedNodeType;
+		}
 
 		if (object instanceof Path) {
 			return true;
 		}
 
 		IResource resource = AdapterUtils.adaptToAnyResource(object);
-		if (resource != null && typeOk(resource))
+		if (resource != null && typeOk(resource)) {
 			return true;
+		}
 
 		return Adapters.adapt(object, Repository.class) != null;
 	}

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -24,6 +24,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
@@ -72,6 +73,7 @@ import org.eclipse.egit.ui.internal.repository.tree.FolderNode;
 import org.eclipse.egit.ui.internal.repository.tree.RefNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNodeType;
 import org.eclipse.egit.ui.internal.repository.tree.TagNode;
 import org.eclipse.egit.ui.internal.selection.RepositorySelectionProvider;
 import org.eclipse.egit.ui.internal.selection.SelectionUtils;
@@ -193,6 +195,12 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 	private static final int INITIAL_ITEM = -1;
 
 	private static final String P_REPOSITORY = "GitHistoryPage.Repository"; //$NON-NLS-1$
+
+	private static final EnumSet SUPPORTED_REPOSITORY_NODE_TYPES = EnumSet.of(
+			RepositoryTreeNodeType.REPO, RepositoryTreeNodeType.REF,
+			RepositoryTreeNodeType.ADDITIONALREF, RepositoryTreeNodeType.TAG,
+			RepositoryTreeNodeType.FOLDER, RepositoryTreeNodeType.FILE,
+			RepositoryTreeNodeType.WORKINGDIR);
 
 	/** actions used in GitHistoryPage **/
 	private static class GitHistoryPageActions {
@@ -1015,22 +1023,27 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 	 *         FOLDER or PROJECT and we can show it; false otherwise.
 	 */
 	public static boolean canShowHistoryFor(final Object object) {
-		if (object instanceof HistoryPageInput)
+		if (object instanceof HistoryPageInput) {
 			return true;
+		}
 
-		if (object instanceof IResource)
+		if (object instanceof IResource) {
 			return typeOk((IResource) object);
+		}
 
-		if (object instanceof RepositoryTreeNode)
-			return true;
+		if (object instanceof RepositoryTreeNode) {
+			return SUPPORTED_REPOSITORY_NODE_TYPES
+					.contains(((RepositoryTreeNode) object).getType());
+		}
 
 		if (object instanceof Path) {
 			return true;
 		}
 
 		IResource resource = AdapterUtils.adaptToAnyResource(object);
-		if (resource != null && typeOk(resource))
+		if (resource != null && typeOk(resource)) {
 			return true;
+		}
 
 		return Adapters.adapt(object, Repository.class) != null;
 	}