eclipse-egit/egit

Use HashMap.computeIfAbsent()
Simplifies code a little, and may be sometimes be a tiny bit faster
since it hashes and finds an entry only once. In IndexDiffCache, it
also enables us to get rid of explicit synchronized blocks and use
a ConcurrentHashMap instead.

Change-Id: Ibf99dec71c62ee682fff9fdad7f06f7232db37b2
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.egit.core/src/org/eclipse/egit/core/internal/ProjectReferenceImporter.java
@@ -74,7 +74,7 @@ public class ProjectReferenceImporter {
 
 		final Map<URIish, Map<String, Set<ProjectReference>>> repositories = parseReferenceStrings();
 
-		final List<IProject> importedProjects = new ArrayList<IProject>();
+		final List<IProject> importedProjects = new ArrayList<>();
 
 		SubMonitor progress = SubMonitor.convert(monitor, repositories.size());
 		for (final Map.Entry<URIish, Map<String, Set<ProjectReference>>> entry : repositories
@@ -154,7 +154,7 @@ public class ProjectReferenceImporter {
 
 	private Map<URIish, Map<String, Set<ProjectReference>>> parseReferenceStrings()
 			throws TeamException {
-		final Map<URIish, Map<String, Set<ProjectReference>>> repositories = new LinkedHashMap<URIish, Map<String, Set<ProjectReference>>>();
+		final Map<URIish, Map<String, Set<ProjectReference>>> repositories = new LinkedHashMap<>();
 
 		for (final String reference : referenceStrings) {
 			if (reference == null) {
@@ -165,21 +165,11 @@ public class ProjectReferenceImporter {
 			try {
 				final ProjectReference projectReference = new ProjectReference(
 						reference);
-				Map<String, Set<ProjectReference>> repositoryBranches = repositories
-						.get(projectReference.getRepository());
-				if (repositoryBranches == null) {
-					repositoryBranches = new HashMap<>();
-					repositories.put(projectReference.getRepository(),
-							repositoryBranches);
-				}
-				Set<ProjectReference> projectReferences = repositoryBranches
-						.get(projectReference.getBranch());
-				if (projectReferences == null) {
-					projectReferences = new LinkedHashSet<>();
-					repositoryBranches.put(projectReference.getBranch(),
-							projectReferences);
-				}
-
+				Set<ProjectReference> projectReferences = repositories
+						.computeIfAbsent(projectReference.getRepository(),
+								key -> new HashMap<>())
+						.computeIfAbsent(projectReference.getBranch(),
+								key -> new LinkedHashSet<>());
 				projectReferences.add(projectReference);
 			} catch (final IllegalArgumentException | URISyntaxException e) {
 				throw new TeamException(reference, e);
@@ -285,7 +275,7 @@ public class ProjectReferenceImporter {
 			final IProgressMonitor monitor) throws TeamException {
 		try {
 
-			List<IProject> importedProjects = new ArrayList<IProject>();
+			List<IProject> importedProjects = new ArrayList<>();
 
 			// import projects from the current repository to workspace
 			final IWorkspace workspace = ResourcesPlugin.getWorkspace();

org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCache.java
@@ -16,10 +16,10 @@ package org.eclipse.egit.core.internal.indexdiff;
 import java.io.File;
 import java.net.URI;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.eclipse.core.filebuffers.FileBuffers;
@@ -48,7 +48,7 @@ import org.eclipse.jgit.lib.Repository;
  */
 public class IndexDiffCache {
 
-	private Map<File, IndexDiffCacheEntry> entries = new HashMap<>();
+	private Map<File, IndexDiffCacheEntry> entries = new ConcurrentHashMap<>();
 
 	private CopyOnWriteArrayList<IndexDiffChangedListener> listeners = new CopyOnWriteArrayList<>();
 
@@ -226,21 +226,13 @@ public class IndexDiffCache {
 	 */
 	@Nullable
 	public IndexDiffCacheEntry getIndexDiffCacheEntry(@NonNull Repository repository) {
-		IndexDiffCacheEntry entry;
-		synchronized (entries) {
-			File gitDir = new Path(repository.getDirectory().getAbsolutePath())
-					.toFile();
-			entry = entries.get(gitDir);
-			if (entry != null) {
-				return entry;
-			}
-			if (repository.isBare()) {
-				return null;
-			}
-			entry = new IndexDiffCacheEntry(repository, globalListener);
-			entries.put(gitDir, entry);
+		if (repository.isBare()) {
+			return null;
 		}
-		return entry;
+		File gitDir = new Path(repository.getDirectory().getAbsolutePath())
+				.toFile();
+		return entries.computeIfAbsent(gitDir,
+				key -> new IndexDiffCacheEntry(repository, globalListener));
 	}
 
 	/**
@@ -281,9 +273,8 @@ public class IndexDiffCache {
 				bufferListener = null;
 			}
 		}
-		for (IndexDiffCacheEntry entry : entries.values()) {
-			entry.dispose();
-		}
+		entries.forEach((k, v) -> v.dispose());
+		entries.clear();
 		Job.getJobManager().cancel(JobFamilies.INDEX_DIFF_CACHE_UPDATE);
 		try {
 			Job.getJobManager().join(JobFamilies.INDEX_DIFF_CACHE_UPDATE, null);
@@ -299,11 +290,9 @@ public class IndexDiffCache {
 	 *            of the {@link Repository} to remove the cache entry of
 	 */
 	public void remove(@NonNull File gitDir) {
-		synchronized (entries) {
-			IndexDiffCacheEntry cachedEntry = entries.remove(gitDir);
-			if (cachedEntry != null) {
-				cachedEntry.dispose();
-			}
+		IndexDiffCacheEntry cachedEntry = entries.remove(gitDir);
+		if (cachedEntry != null) {
+			cachedEntry.dispose();
 		}
 	}
 
@@ -316,11 +305,7 @@ public class IndexDiffCache {
 	 */
 	@NonNull
 	public Set<File> currentCacheEntries() {
-		Set<File> result = null;
-		synchronized (entries) {
-			result = new HashSet<>(entries.keySet());
-		}
-		return result;
+		return new HashSet<>(entries.keySet());
 	}
 
 }

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchResultTable.java
@@ -450,16 +450,13 @@ class FetchResultTable {
 	}
 
 	private String safeAbbreviate(ObjectId id) {
-		String abbrev = abbrevations.get(id);
-		if (abbrev == null) {
+		return abbrevations.computeIfAbsent(id, x -> {
 			try {
-				abbrev = reader.abbreviate(id).name();
+				return reader.abbreviate(x).name();
 			} catch (IOException cannotAbbreviate) {
-				abbrev = id.name();
+				return x.name();
 			}
-			abbrevations.put(id, abbrev);
-		}
-		return abbrev;
+		});
 	}
 
 	Control getControl() {

ShoOgino/egitFile

org.eclipse.egit.core/src/org/eclipse/egit/core/internal/ProjectReferenceImporter.java
@@ -74,7 +74,7 @@ public class ProjectReferenceImporter {
 
 		final Map<URIish, Map<String, Set<ProjectReference>>> repositories = parseReferenceStrings();
 
-		final List<IProject> importedProjects = new ArrayList<IProject>();
+		final List<IProject> importedProjects = new ArrayList<>();
 
 		SubMonitor progress = SubMonitor.convert(monitor, repositories.size());
 		for (final Map.Entry<URIish, Map<String, Set<ProjectReference>>> entry : repositories
@@ -154,7 +154,7 @@ public class ProjectReferenceImporter {
 
 	private Map<URIish, Map<String, Set<ProjectReference>>> parseReferenceStrings()
 			throws TeamException {
-		final Map<URIish, Map<String, Set<ProjectReference>>> repositories = new LinkedHashMap<URIish, Map<String, Set<ProjectReference>>>();
+		final Map<URIish, Map<String, Set<ProjectReference>>> repositories = new LinkedHashMap<>();
 
 		for (final String reference : referenceStrings) {
 			if (reference == null) {
@@ -165,21 +165,11 @@ public class ProjectReferenceImporter {
 			try {
 				final ProjectReference projectReference = new ProjectReference(
 						reference);
-				Map<String, Set<ProjectReference>> repositoryBranches = repositories
-						.get(projectReference.getRepository());
-				if (repositoryBranches == null) {
-					repositoryBranches = new HashMap<>();
-					repositories.put(projectReference.getRepository(),
-							repositoryBranches);
-				}
-				Set<ProjectReference> projectReferences = repositoryBranches
-						.get(projectReference.getBranch());
-				if (projectReferences == null) {
-					projectReferences = new LinkedHashSet<>();
-					repositoryBranches.put(projectReference.getBranch(),
-							projectReferences);
-				}
-
+				Set<ProjectReference> projectReferences = repositories
+						.computeIfAbsent(projectReference.getRepository(),
+								repo -> new HashMap<>())
+						.computeIfAbsent(projectReference.getBranch(),
+								branch -> new LinkedHashSet<>());
 				projectReferences.add(projectReference);
 			} catch (final IllegalArgumentException | URISyntaxException e) {
 				throw new TeamException(reference, e);
@@ -285,7 +275,7 @@ public class ProjectReferenceImporter {
 			final IProgressMonitor monitor) throws TeamException {
 		try {
 
-			List<IProject> importedProjects = new ArrayList<IProject>();
+			List<IProject> importedProjects = new ArrayList<>();
 
 			// import projects from the current repository to workspace
 			final IWorkspace workspace = ResourcesPlugin.getWorkspace();

org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCache.java
@@ -16,10 +16,10 @@ package org.eclipse.egit.core.internal.indexdiff;
 import java.io.File;
 import java.net.URI;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.eclipse.core.filebuffers.FileBuffers;
@@ -48,7 +48,7 @@ import org.eclipse.jgit.lib.Repository;
  */
 public class IndexDiffCache {
 
-	private Map<File, IndexDiffCacheEntry> entries = new HashMap<>();
+	private Map<File, IndexDiffCacheEntry> entries = new ConcurrentHashMap<>();
 
 	private CopyOnWriteArrayList<IndexDiffChangedListener> listeners = new CopyOnWriteArrayList<>();
 
@@ -226,21 +226,13 @@ public class IndexDiffCache {
 	 */
 	@Nullable
 	public IndexDiffCacheEntry getIndexDiffCacheEntry(@NonNull Repository repository) {
-		IndexDiffCacheEntry entry;
-		synchronized (entries) {
-			File gitDir = new Path(repository.getDirectory().getAbsolutePath())
-					.toFile();
-			entry = entries.get(gitDir);
-			if (entry != null) {
-				return entry;
-			}
-			if (repository.isBare()) {
-				return null;
-			}
-			entry = new IndexDiffCacheEntry(repository, globalListener);
-			entries.put(gitDir, entry);
+		if (repository.isBare()) {
+			return null;
 		}
-		return entry;
+		File gitDir = new Path(repository.getDirectory().getAbsolutePath())
+				.toFile();
+		return entries.computeIfAbsent(gitDir,
+				dir -> new IndexDiffCacheEntry(repository, globalListener));
 	}
 
 	/**
@@ -281,9 +273,8 @@ public class IndexDiffCache {
 				bufferListener = null;
 			}
 		}
-		for (IndexDiffCacheEntry entry : entries.values()) {
-			entry.dispose();
-		}
+		entries.forEach((gitDir, entry) -> entry.dispose());
+		entries.clear();
 		Job.getJobManager().cancel(JobFamilies.INDEX_DIFF_CACHE_UPDATE);
 		try {
 			Job.getJobManager().join(JobFamilies.INDEX_DIFF_CACHE_UPDATE, null);
@@ -299,11 +290,9 @@ public class IndexDiffCache {
 	 *            of the {@link Repository} to remove the cache entry of
 	 */
 	public void remove(@NonNull File gitDir) {
-		synchronized (entries) {
-			IndexDiffCacheEntry cachedEntry = entries.remove(gitDir);
-			if (cachedEntry != null) {
-				cachedEntry.dispose();
-			}
+		IndexDiffCacheEntry cachedEntry = entries.remove(gitDir);
+		if (cachedEntry != null) {
+			cachedEntry.dispose();
 		}
 	}
 
@@ -316,11 +305,7 @@ public class IndexDiffCache {
 	 */
 	@NonNull
 	public Set<File> currentCacheEntries() {
-		Set<File> result = null;
-		synchronized (entries) {
-			result = new HashSet<>(entries.keySet());
-		}
-		return result;
+		return new HashSet<>(entries.keySet());
 	}
 
 }

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchResultTable.java
@@ -449,17 +449,14 @@ class FetchResultTable {
 		treeViewer.setInput(fetchResult);
 	}
 
-	private String safeAbbreviate(ObjectId id) {
-		String abbrev = abbrevations.get(id);
-		if (abbrev == null) {
+	private String safeAbbreviate(ObjectId objectId) {
+		return abbrevations.computeIfAbsent(objectId, id -> {
 			try {
-				abbrev = reader.abbreviate(id).name();
+				return reader.abbreviate(id).name();
 			} catch (IOException cannotAbbreviate) {
-				abbrev = id.name();
+				return id.name();
 			}
-			abbrevations.put(id, abbrev);
-		}
-		return abbrev;
+		});
 	}
 
 	Control getControl() {