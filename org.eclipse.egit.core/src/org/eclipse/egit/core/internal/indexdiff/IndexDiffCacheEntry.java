eclipse-egit/egit

Use cached repositories for submodules
Use the new RepositoryBuilderFactory from JGit to make sure diffs
for submodules use the config-caching aware Repository instances
from the EGit RepositoryCache instead of new Repository instances.

Bug: 550878
JGit-Dependency: Icc1b34dfc4cebd8ed4739dd09d37744d41adf711
Change-Id: I6817f45139589f1db69d3bdd20ca786f1f2746fe
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCacheEntry.java
@@ -49,6 +49,7 @@ import org.eclipse.egit.core.ConfigScope;
 import org.eclipse.egit.core.EclipseGitProgressTransformer;
 import org.eclipse.egit.core.IteratorService;
 import org.eclipse.egit.core.JobFamilies;
+import org.eclipse.egit.core.RepositoryCache;
 import org.eclipse.egit.core.internal.CoreText;
 import org.eclipse.egit.core.internal.SafeRunnable;
 import org.eclipse.egit.core.internal.job.RuleUtil;
@@ -158,23 +159,22 @@ public class IndexDiffCacheEntry {
 				.addRefsChangedListener(refsChangedListener));
 		// Add a listener also to all submodules in order to be notified when
 		// a branch switch or so occurs in a submodule.
+		RepositoryCache cache = org.eclipse.egit.core.Activator.getDefault()
+				.getRepositoryCache();
 		try (ConfigScope scope = new ConfigScope(repository);
 				SubmoduleWalk walk = SubmoduleWalk.forIndex(repository)) {
+			walk.setBuilderFactory(() -> cache.getBuilder(false, false));
 			while (walk.next()) {
 				Repository submodule = walk.getRepository();
 				if (submodule != null && !submodule.isBare()) {
-					Repository cached = org.eclipse.egit.core.Activator
-							.getDefault().getRepositoryCache().lookupRepository(
-									submodule.getDirectory().getAbsoluteFile());
-					submodules.put(cached, walk.getPath());
+					submodules.put(submodule, walk.getPath());
 					IndexDiffCacheEntry submoduleCache = org.eclipse.egit.core.Activator
 							.getDefault().getIndexDiffCache()
-							.getIndexDiffCacheEntry(cached);
+							.getIndexDiffCacheEntry(submodule);
 					if (submoduleCache != null) {
 						submoduleCache
 								.addIndexDiffChangedListener(submoduleListener);
 					}
-					submodule.close();
 				}
 			}
 		} catch (IOException ex) {
@@ -626,7 +626,9 @@ public class IndexDiffCacheEntry {
 					Constants.HEAD, iterator);
 			diffForChangedResources.setFilter(
 					PathFilterGroup.createFromStrings(treeFilterPaths));
-			diffForChangedResources.diff(jgitMonitor, 0, 0, jobName);
+			diffForChangedResources.diff(jgitMonitor, 0, 0, jobName,
+					() -> Activator.getDefault().getRepositoryCache()
+							.getBuilder(true, true));
 			IndexDiffData previous = indexDiffData;
 			if (previous == null) {
 				// Can happen when the index diff cache entry is already
@@ -679,9 +681,10 @@ public class IndexDiffCacheEntry {
 			if (iterator == null)
 				return null; // workspace is closed
 			newIndexDiff = new IndexDiff(repository, Constants.HEAD, iterator);
-			newIndexDiff.diff(jgitMonitor, 0, 0, jobName);
-			return new IndexDiffData(newIndexDiff);
+			newIndexDiff.diff(jgitMonitor, 0, 0, jobName, () -> Activator
+					.getDefault().getRepositoryCache().getBuilder(true, true));
 		}
+		return new IndexDiffData(newIndexDiff);
 	}
 
 	private String getReloadJobName() {

ShoOgino/egitFile

org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCacheEntry.java
@@ -48,6 +48,7 @@ import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.EclipseGitProgressTransformer;
 import org.eclipse.egit.core.IteratorService;
 import org.eclipse.egit.core.JobFamilies;
+import org.eclipse.egit.core.RepositoryCache;
 import org.eclipse.egit.core.UnitOfWork;
 import org.eclipse.egit.core.internal.CoreText;
 import org.eclipse.egit.core.internal.SafeRunnable;
@@ -158,25 +159,23 @@ public class IndexDiffCacheEntry {
 				.addRefsChangedListener(refsChangedListener));
 		// Add a listener also to all submodules in order to be notified when
 		// a branch switch or so occurs in a submodule.
+		RepositoryCache cache = org.eclipse.egit.core.Activator.getDefault()
+				.getRepositoryCache();
 		try {
 			UnitOfWork.run(repository, () -> {
 				try (SubmoduleWalk walk = SubmoduleWalk.forIndex(repository)) {
+					walk.setBuilderFactory(() -> cache.getBuilder(false, false));
 					while (walk.next()) {
 						Repository submodule = walk.getRepository();
 						if (submodule != null && !submodule.isBare()) {
-							Repository cached = org.eclipse.egit.core.Activator
-									.getDefault().getRepositoryCache()
-									.lookupRepository(submodule.getDirectory()
-											.getAbsoluteFile());
-							submodules.put(cached, walk.getPath());
+							submodules.put(submodule, walk.getPath());
 							IndexDiffCacheEntry submoduleCache = org.eclipse.egit.core.Activator
 									.getDefault().getIndexDiffCache()
-									.getIndexDiffCacheEntry(cached);
+									.getIndexDiffCacheEntry(submodule);
 							if (submoduleCache != null) {
 								submoduleCache.addIndexDiffChangedListener(
 										submoduleListener);
 							}
-							submodule.close();
 						}
 					}
 				}
@@ -630,7 +629,9 @@ public class IndexDiffCacheEntry {
 					Constants.HEAD, iterator);
 			diffForChangedResources.setFilter(
 					PathFilterGroup.createFromStrings(treeFilterPaths));
-			diffForChangedResources.diff(jgitMonitor, 0, 0, jobName);
+			diffForChangedResources.diff(jgitMonitor, 0, 0, jobName,
+					() -> Activator.getDefault().getRepositoryCache()
+							.getBuilder(true, true));
 			IndexDiffData previous = indexDiffData;
 			if (previous == null) {
 				// Can happen when the index diff cache entry is already
@@ -683,7 +684,8 @@ public class IndexDiffCacheEntry {
 				return null; // workspace is closed
 			IndexDiff newDiff = new IndexDiff(repository, Constants.HEAD,
 					iterator);
-			newDiff.diff(jgitMonitor, 0, 0, jobName);
+			newDiff.diff(jgitMonitor, 0, 0, jobName, () -> Activator
+					.getDefault().getRepositoryCache().getBuilder(true, true));
 			return new IndexDiffData(newDiff);
 		});
 	}