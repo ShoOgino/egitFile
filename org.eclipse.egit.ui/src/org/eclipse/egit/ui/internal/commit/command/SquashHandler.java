eclipse-egit/egit

CommitMessageEditorDialog: allow clients to specify button labels
The used button labels are appropriate for most situations. But when
called from SquashHandler, the Cancel label made users expect that the
whole squash operation would be cancelled (which is not the case). To
improve the dialog in this case, allow clients to specify the button
labels.

Bug: 451817
Change-Id: I66fb344356ecc69c07a1c3d4a73c52f4434376d5
Signed-off-by: Simon Muschel <smuschel@gmx.de>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/command/SquashHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Maik Schreiber
+ * Copyright (c) 2014, 2019 Maik Schreiber
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10,6 +10,7 @@
  *
  * Contributors:
  *    Maik Schreiber - initial implementation
+ *    Simon Muschel <smuschel@gmx.de> - Bug 451817
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.commit.command;
 
@@ -111,7 +112,9 @@ public class SquashHandler extends SelectionHandler {
 			@Override
 			public void run() {
 				CommitMessageEditorDialog dialog = new CommitMessageEditorDialog(
-						shell, msg[0]);
+						shell, msg[0],
+						UIText.RebaseInteractiveHandler_EditMessageDialogOkButton,
+						UIText.SquashHandler_EditMessageDialogCancelButton);
 				if (dialog.open() == Window.OK)
 					msg[0] = dialog.getCommitMessage();
 				else

ShoOgino/egitFile

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/command/SquashHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Maik Schreiber
+ * Copyright (c) 2014, 2019 Maik Schreiber
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10,6 +10,7 @@
  *
  * Contributors:
  *    Maik Schreiber - initial implementation
+ *    Simon Muschel <smuschel@gmx.de> - Bug 451817
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.commit.command;
 
@@ -111,7 +112,9 @@ public class SquashHandler extends SelectionHandler {
 			@Override
 			public void run() {
 				CommitMessageEditorDialog dialog = new CommitMessageEditorDialog(
-						shell, msg[0]);
+						shell, msg[0],
+						UIText.CommitMessageEditorDialog_OkButton,
+						UIText.SquashHandler_EditMessageDialogCancelButton);
 				if (dialog.open() == Window.OK)
 					msg[0] = dialog.getCommitMessage();
 				else