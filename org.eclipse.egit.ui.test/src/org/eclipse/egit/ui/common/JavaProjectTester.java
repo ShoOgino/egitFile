eclipse-egit/egit

Fix typo in tests
Change-Id: I7c91f36a166b371e688c2c598393da87d3299f85
Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/JavaProjectTester.java
@@ -98,11 +98,11 @@ public class JavaProjectTester {
 				.getFolder(PACKAGE_NAME);
 		IFile file = folder.getFile(JAVA_FILE_NAME);
 
-		IFile[] commitables = new IFile[] { file };
+		IFile[] committables = new IFile[] { file };
 		ArrayList<IFile> untracked = new ArrayList<>();
-		untracked.addAll(Arrays.asList(commitables));
+		untracked.addAll(Arrays.asList(committables));
 		// commit to master
-		CommitOperation op = new CommitOperation(commitables, untracked,
+		CommitOperation op = new CommitOperation(committables, untracked,
 				TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER, "Initial commit");
 		op.execute(null);
 

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/LocalRepositoryTestCase.java
@@ -379,21 +379,21 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 		IFolder folder = firstProject.getFolder(FOLDER);
 		IFile textFile = folder.getFile(FILE1);
 		IFile textFile2 = folder.getFile(FILE2);
-		IFile[] commitables = null;
+		IFile[] committables = null;
 		if (secondProject != null) {
 			folder = secondProject.getFolder(FOLDER);
 			IFile secondtextFile = folder.getFile(FILE1);
 			IFile secondtextFile2 = folder.getFile(FILE2);
 
-			commitables = new IFile[] { dotProject, textFile, textFile2,
+			committables = new IFile[] { dotProject, textFile, textFile2,
 					secondtextFile, secondtextFile2 };
 		} else {
-			commitables = new IFile[] { dotProject, textFile, textFile2 };
+			committables = new IFile[] { dotProject, textFile, textFile2 };
 		}
 		ArrayList<IFile> untracked = new ArrayList<>();
-		untracked.addAll(Arrays.asList(commitables));
+		untracked.addAll(Arrays.asList(committables));
 		// commit to stable
-		CommitOperation op = new CommitOperation(commitables,
+		CommitOperation op = new CommitOperation(committables,
 				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
 				"Initial commit");
 		op.execute(null);
@@ -688,13 +688,13 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 			throws Exception {
 		IFile file = touch(newContent);
 
-		IFile[] commitables = new IFile[] { file };
+		IFile[] committables = new IFile[] { file };
 		ArrayList<IFile> untracked = new ArrayList<>();
-		untracked.addAll(Arrays.asList(commitables));
+		untracked.addAll(Arrays.asList(committables));
 		String message = commitMessage;
 		if (message == null)
 			message = newContent;
-		CommitOperation op = new CommitOperation(commitables,
+		CommitOperation op = new CommitOperation(committables,
 				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
 				message);
 		op.execute(null);
@@ -754,10 +754,10 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 		IProject prj = file.getProject();
 		if (!prj.isAccessible())
 			throw new IllegalStateException("No project to touch");
-		IFile[] commitables = new IFile[] { file };
+		IFile[] committables = new IFile[] { file };
 		ArrayList<IFile> untracked = new ArrayList<>();
-		untracked.addAll(Arrays.asList(commitables));
-		CommitOperation op = new CommitOperation(commitables,
+		untracked.addAll(Arrays.asList(committables));
+		CommitOperation op = new CommitOperation(committables,
 				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
 				commitMessage);
 		op.execute(null);

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/CreatePatchActionTest.java
@@ -91,9 +91,9 @@ public class CreatePatchActionTest extends LocalRepositoryTestCase {
 	public void setup() throws Exception {
 		createProjectAndCommitToRepository();
 
-		IFile[] commitables = getAllFiles();
-		CommitOperation cop = new CommitOperation(commitables,
-				Arrays.asList(commitables), TestUtil.TESTAUTHOR,
+		IFile[] committables = getAllFiles();
+		CommitOperation cop = new CommitOperation(committables,
+				Arrays.asList(committables), TestUtil.TESTAUTHOR,
 				TestUtil.TESTCOMMITTER, "Initial commit");
 		cop.setAmending(true);
 		cop.execute(null);

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/synchronize/AbstractSynchronizeViewTest.java
@@ -258,11 +258,11 @@ public abstract class AbstractSynchronizeViewTest extends
 				new ByteArrayInputStream("/.project\n".getBytes(secondPoject
 						.getDefaultCharset())), false, null);
 
-		IFile[] commitables = new IFile[] { gitignore };
+		IFile[] committables = new IFile[] { gitignore };
 		ArrayList<IFile> untracked = new ArrayList<IFile>();
-		untracked.addAll(Arrays.asList(commitables));
+		untracked.addAll(Arrays.asList(committables));
 
-		CommitOperation op = new CommitOperation(commitables,
+		CommitOperation op = new CommitOperation(committables,
 				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
 				"Add .gitignore file");
 		op.execute(null);

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/synchronize/SynchronizeViewRemoteAwareChangeSetModelTest.java
@@ -177,10 +177,10 @@ public class SynchronizeViewRemoteAwareChangeSetModelTest
 						"Content 2".getBytes(project.getDefaultCharset())),
 				false, null);
 
-		IFile[] commitables = new IFile[] { mockLogicalFile, file1, file2 };
+		IFile[] committables = new IFile[] { mockLogicalFile, file1, file2 };
 		List<IFile> untracked = new ArrayList<>();
-		untracked.addAll(Arrays.asList(commitables));
-		CommitOperation op = new CommitOperation(commitables, untracked,
+		untracked.addAll(Arrays.asList(committables));
+		CommitOperation op = new CommitOperation(committables, untracked,
 				TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER, "Initial commit");
 		op.execute(null);
 		RevCommit firstCommit = op.getCommit();
@@ -199,10 +199,10 @@ public class SynchronizeViewRemoteAwareChangeSetModelTest
 				new ByteArrayInputStream(
 						"Content 3".getBytes(project.getDefaultCharset())),
 				false, null);
-		commitables = new IFile[] { mockLogicalFile, file1, file2, file3 };
+		committables = new IFile[] { mockLogicalFile, file1, file2, file3 };
 		untracked = new ArrayList<>();
 		untracked.add(file3);
-		op = new CommitOperation(commitables, untracked, TestUtil.TESTAUTHOR,
+		op = new CommitOperation(committables, untracked, TestUtil.TESTAUTHOR,
 				TestUtil.TESTCOMMITTER, "Second commit");
 		op.execute(null);
 	}

ShoOgino/egitFile

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/JavaProjectTester.java
@@ -98,11 +98,11 @@ public class JavaProjectTester {
 				.getFolder(PACKAGE_NAME);
 		IFile file = folder.getFile(JAVA_FILE_NAME);
 
-		IFile[] commitables = new IFile[] { file };
+		IFile[] committableFiles = new IFile[] { file };
 		ArrayList<IFile> untracked = new ArrayList<>();
-		untracked.addAll(Arrays.asList(commitables));
+		untracked.addAll(Arrays.asList(committableFiles));
 		// commit to master
-		CommitOperation op = new CommitOperation(commitables, untracked,
+		CommitOperation op = new CommitOperation(committableFiles, untracked,
 				TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER, "Initial commit");
 		op.execute(null);
 

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/LocalRepositoryTestCase.java
@@ -379,21 +379,21 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 		IFolder folder = firstProject.getFolder(FOLDER);
 		IFile textFile = folder.getFile(FILE1);
 		IFile textFile2 = folder.getFile(FILE2);
-		IFile[] commitables = null;
+		IFile[] committableFiles = null;
 		if (secondProject != null) {
 			folder = secondProject.getFolder(FOLDER);
 			IFile secondtextFile = folder.getFile(FILE1);
 			IFile secondtextFile2 = folder.getFile(FILE2);
 
-			commitables = new IFile[] { dotProject, textFile, textFile2,
+			committableFiles = new IFile[] { dotProject, textFile, textFile2,
 					secondtextFile, secondtextFile2 };
 		} else {
-			commitables = new IFile[] { dotProject, textFile, textFile2 };
+			committableFiles = new IFile[] { dotProject, textFile, textFile2 };
 		}
 		ArrayList<IFile> untracked = new ArrayList<>();
-		untracked.addAll(Arrays.asList(commitables));
+		untracked.addAll(Arrays.asList(committableFiles));
 		// commit to stable
-		CommitOperation op = new CommitOperation(commitables,
+		CommitOperation op = new CommitOperation(committableFiles,
 				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
 				"Initial commit");
 		op.execute(null);
@@ -688,13 +688,13 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 			throws Exception {
 		IFile file = touch(newContent);
 
-		IFile[] commitables = new IFile[] { file };
+		IFile[] committableFiles = new IFile[] { file };
 		ArrayList<IFile> untracked = new ArrayList<>();
-		untracked.addAll(Arrays.asList(commitables));
+		untracked.addAll(Arrays.asList(committableFiles));
 		String message = commitMessage;
 		if (message == null)
 			message = newContent;
-		CommitOperation op = new CommitOperation(commitables,
+		CommitOperation op = new CommitOperation(committableFiles,
 				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
 				message);
 		op.execute(null);
@@ -754,10 +754,10 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 		IProject prj = file.getProject();
 		if (!prj.isAccessible())
 			throw new IllegalStateException("No project to touch");
-		IFile[] commitables = new IFile[] { file };
+		IFile[] committableFiles = new IFile[] { file };
 		ArrayList<IFile> untracked = new ArrayList<>();
-		untracked.addAll(Arrays.asList(commitables));
-		CommitOperation op = new CommitOperation(commitables,
+		untracked.addAll(Arrays.asList(committableFiles));
+		CommitOperation op = new CommitOperation(committableFiles,
 				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
 				commitMessage);
 		op.execute(null);

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/CreatePatchActionTest.java
@@ -91,9 +91,9 @@ public class CreatePatchActionTest extends LocalRepositoryTestCase {
 	public void setup() throws Exception {
 		createProjectAndCommitToRepository();
 
-		IFile[] commitables = getAllFiles();
-		CommitOperation cop = new CommitOperation(commitables,
-				Arrays.asList(commitables), TestUtil.TESTAUTHOR,
+		IFile[] committableFiles = getAllFiles();
+		CommitOperation cop = new CommitOperation(committableFiles,
+				Arrays.asList(committableFiles), TestUtil.TESTAUTHOR,
 				TestUtil.TESTCOMMITTER, "Initial commit");
 		cop.setAmending(true);
 		cop.execute(null);

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/synchronize/AbstractSynchronizeViewTest.java
@@ -258,11 +258,11 @@ public abstract class AbstractSynchronizeViewTest extends
 				new ByteArrayInputStream("/.project\n".getBytes(secondPoject
 						.getDefaultCharset())), false, null);
 
-		IFile[] commitables = new IFile[] { gitignore };
+		IFile[] committableFiles = new IFile[] { gitignore };
 		ArrayList<IFile> untracked = new ArrayList<IFile>();
-		untracked.addAll(Arrays.asList(commitables));
+		untracked.addAll(Arrays.asList(committableFiles));
 
-		CommitOperation op = new CommitOperation(commitables,
+		CommitOperation op = new CommitOperation(committableFiles,
 				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
 				"Add .gitignore file");
 		op.execute(null);

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/synchronize/SynchronizeViewRemoteAwareChangeSetModelTest.java
@@ -177,10 +177,10 @@ public class SynchronizeViewRemoteAwareChangeSetModelTest
 						"Content 2".getBytes(project.getDefaultCharset())),
 				false, null);
 
-		IFile[] commitables = new IFile[] { mockLogicalFile, file1, file2 };
+		IFile[] committableFiles = new IFile[] { mockLogicalFile, file1, file2 };
 		List<IFile> untracked = new ArrayList<>();
-		untracked.addAll(Arrays.asList(commitables));
-		CommitOperation op = new CommitOperation(commitables, untracked,
+		untracked.addAll(Arrays.asList(committableFiles));
+		CommitOperation op = new CommitOperation(committableFiles, untracked,
 				TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER, "Initial commit");
 		op.execute(null);
 		RevCommit firstCommit = op.getCommit();
@@ -199,10 +199,10 @@ public class SynchronizeViewRemoteAwareChangeSetModelTest
 				new ByteArrayInputStream(
 						"Content 3".getBytes(project.getDefaultCharset())),
 				false, null);
-		commitables = new IFile[] { mockLogicalFile, file1, file2, file3 };
+		committableFiles = new IFile[] { mockLogicalFile, file1, file2, file3 };
 		untracked = new ArrayList<>();
 		untracked.add(file3);
-		op = new CommitOperation(commitables, untracked, TestUtil.TESTAUTHOR,
+		op = new CommitOperation(committableFiles, untracked, TestUtil.TESTAUTHOR,
 				TestUtil.TESTCOMMITTER, "Second commit");
 		op.execute(null);
 	}